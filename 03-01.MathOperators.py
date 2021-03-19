e06bf6998ad5f311774bb5d639378394f23b3576
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-03-20 00:42:15 +0300

    Update and rename 03-01.Numbers.py to 03-01.MathOperators.py

--- 03-01.MathOperators.py
+++ 03-01.MathOperators.py
@@ -31,3 +31,18 @@ num = 17
 print(num % x)
 
 numbers[::-1] - #listeyi ters dizer
+
+-----------
+#matematiksel operatörler
+# ** - üs 
+# % - mod
+# // - tam bölme
+
+4-0.5
+0.5*2
+2**3
+3**0.5
+10%2    #10nun 2ye bölümünden kalan 0
+10/3    #3.333333 
+#eğer 10//3 dersek ondalıklı kısmı atar tan kısmı yani 3ü gösteir
+-15//4
