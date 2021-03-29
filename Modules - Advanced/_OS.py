42bde0fe7dccf649500e7533d5f523910a375f20
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-03-29 07:18:50 +0300

    Update _OS.py

--- Modules - Advanced/_OS.py
+++ Modules - Advanced/_OS.py
@@ -1,3 +1,61 @@
+import os
+import datetime
+
+result = dir(os)
+result = os.name
+
+# dizin değiştirme
+# os.chdir('C:\\')
+# os.chdir('../..')
+
+# klasör oluşturma
+# os.mkdir("newdirectory")
+# os.makedirs("newdirectory/yeniklasör")
+# os.rename("newdirectory","yeniklasör")
+# os.rmdir("newdirectory")
+# os.removedirs("yeniklasör/yeniklasör")
+
+# listeleme
+# result = os.listdir()
+# result = os.listdir('C:\\')
+# for dosya in os.listdir():
+#     if dosya.endswith('.py'):
+#         print(dosya)
+
+
+# etkin dizin öğrenme
+# result = os.getcwd()
+
+
+# result = os.stat("_datetime.py")
+# result = result.st_size/1024
+# result = datetime.datetime.fromtimestamp(result.st_ctime)  # oluşturulma tarihi
+# result = datetime.datetime.fromtimestamp(result.st_atime)  # son erişilme tarihi
+# result = datetime.datetime.fromtimestamp(result.st_mtime)  # değiştirilme tarihi
+
+# os.system("notepad.exe")
+
+# path
+
+result = os.path.abspath("_os.py")
+result = os.path.dirname("C:/python/advanced-modules/_os.py")
+result = os.path.dirname(os.path.abspath("_os.py"))
+result = os.path.exists("C:/python/advanced-modules/_os1.py")
+result = os.path.exists("C:/python/advanced-modules")
+result = os.path.isdir("C:/python/advanced-modules")
+result = os.path.isfile("C:/python/advanced-modules/_os.py")
+result = os.path.join("C:\\","deneme","deneme1")
+result = os.path.split("C:\\deneme")
+result = os.path.splitext("_os.py")
+# result = result[0]
+result = result[1]
+
+print(result)
+
+
+##############################################
+
+
 """
 import os
 result = dir(os)
@@ -99,7 +157,7 @@ result = result[1] #sadece dosya uzantısı gelir
 
 
 ###########################################
-#Code to shutdown a computer in py
+#code to shutdown a machine in py
 import os
 os.system("shutdown /s /t 1")
 
