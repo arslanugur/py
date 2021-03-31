bfb8b57c43fdb00577a6eb026ae13c954cebe442
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-01 02:23:22 +0300

    Update MinimumSpanningTree-Kruskal.py

--- Algorithms/Graph/MinimumSpanningTree-Kruskal.py
+++ Algorithms/Graph/MinimumSpanningTree-Kruskal.py
@@ -43,3 +43,22 @@ if __name__ == "__main__":  # pragma: no cover
         edges.append((node1, node2, cost))
 
     kruskal(num_nodes, num_edges, edges)
+
+    
+"""
+En Kısa Tarama Ağacı - en kısa yoldan bütün düğümleri dolaşan algo
+Kruskal algo edgeler kenarlar üzerinden gider
+düğümler nodes v edges kenarlar
+edge kavramı üzerinden gidersek ilk etapta sıralamamız gerekiyor
+mesela 1 boyutuna sahip değişik yerde bulunan edgeler var  2 3 4 diye gider
+algo aslında öncelikle bir edge list oluşturup küçükten büyüğe doğru sıralıyor
+ardından bu edgeleri alarak devam ediyor. 
+dikkat edilmesi gereken şey mesela x-v:1 dediğimizde x ve v nodeları var ve bunlar birbirine bağlanması söz konusu
+visited x ve unvisited v durumu var
+mesela x y z w diyelim bunlar ilk etapta sıfırdır ama visit edildikçe 1 olur
+edge almak için ya ikisi de unvisited olcak ya da birisi unvisited olcak
+şayet ikisi de visited ise o node alınmaz, yani zaten ziyaret edilmiş bir daha ikisine birden edge almaya gerek yok
+bu algo en kısa şekilde dolaşarak tarama yapmayı sağlar -en az maliyetli  yol diyebiliriz - maliyetini sonuçların toplamını alarak yapabiliriz
+iddiası ise bütün düğümleri dolaşan daha kısa maliyetli bir yol bulunamaz. O yol budur der
+ama bir yerden bir yere gitmenin birden fazla yolu varsa eşit maliyetli olabilir
+"""
