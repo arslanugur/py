55633f8337cefacd1ab9935ab9783986f4b74391
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-02 13:40:39 +0300

    Create K-Means.py

---
+++ Algorithms/Machine Learning/Unsupervised/K-Means.py
@@ -0,0 +1,10 @@
+#Import Library
+from sklearn.cluster import KMeans
+#Assumed u have, X (attributes) for training data set
+#and x_test(attributes) of test_dataset
+#Create KNeighbors classifierobject model
+k_means = KMeans(n_clusters = 3, random_state = 0)
+#Train the model using the training sets and check score
+model.fit(X)
+#Predict Output
+predicted = model.predict(x_test)
