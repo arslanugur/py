6faad5d9fc525642d2196799682d07a6fef7abb8
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-02 14:31:24 +0300

    Update RandomForestClassifier.py

--- Algorithms/Machine Learning/Supervised/RandomForestClassifier.py
+++ Algorithms/Machine Learning/Supervised/RandomForestClassifier.py
@@ -1,3 +1,16 @@
+#Import Library
+from sklearn.ensemble import RandomForestClassifier
+#Assumed u have, X (predictor), and Y (target) for
+#training data set and x_test(predictor) of test_dataset
+#Create Random Forest Object
+model = RandomForestClassifier()
+#Train the model using the training sets and check score
+model.fit(X, Y)
+#Predict Output
+predicted = model.predict(x_test)
+
+##################################
+
 # Random Forest Classifier Example
 from matplotlib import pyplot as plt
 from sklearn.datasets import load_iris
