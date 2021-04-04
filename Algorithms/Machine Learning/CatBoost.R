163d89992b20c4e9fd0406e364f955c32c29104b
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-04 20:00:54 +0300

    Create CatBoost.R

---
+++ Algorithms/Machine Learning/CatBoost.R
@@ -0,0 +1,27 @@
+set.seed(1)
+
+require(titanic)
+
+require(caret)
+
+require(catboost)
+
+tt <- titanic::titanic_train[complete.cases(titanic::titanic_train),]
+
+data <- as.data.frame(as.matrix(tt), stringsAsFactors = TRUE)
+
+drop_columns = c("PassengerId", "Survived", "Name", "Ticket", "Cabin")
+
+x <- data[,!(names(data) %in% drop_columns)]y <- data[,c("Survived")]
+
+fit_control <- trainControl(method = "cv", number = 4,classProbs = TRUE)
+
+grid <- expand.grid(depth = c(4, 6, 8),learning_rate = 0.1,iterations = 100, l2_leaf_reg = 1e-3,            rsm = 0.95, border_count = 64)
+
+report <- train(x, as.factor(make.names(y)),method = catboost.caret,verbose = TRUE, preProc = NULL,tuneGrid = grid, trControl = fit_control)
+
+print(report)
+
+importance <- varImp(report, scale = FALSE)
+
+print(importance)
