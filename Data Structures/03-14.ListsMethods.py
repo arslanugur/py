2b8fbc3ea063b01b0e94454a926c224e2d279fc7
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-08 22:05:03 +0300

    Update 03-14.ListsMethods.py

--- Data Structures/03-14.ListsMethods.py
+++ Data Structures/03-14.ListsMethods.py
@@ -1,47 +1,47 @@
 numbers = [2, 3, 7, 1, 98, 67]
 letters = ['a', 's', 'z', 'e', 'r', 'u']
 
-val = min(numbers) #numbers dizilerinderinde minimum değer
+val = min(numbers)  #numbers dizilerinderinde minimum değer
 val = max(numbers)
 val = max(letters)
 val = min(letters)
 
-val = numbers[3:6] #bunu stringlerde de yapıyoruz
-val = numbers[:4]
-val = numbers[3:]
+val = numbers[3:6]   #bunu stringlerde de yapıyoruz
+val = numbers[:3]
+val = numbers[4:]
 print(val)
 
 numbers[4] = 40 #index nosu 4 olan sayı 40 olur
-print(numbers)
 
-numbers.append('500') #ekleme methodu str olarak eklendi
-print(numbers)
-numbers.append(50) #numnber olarak eklenir
-print(numbers)
+numbers.append(49) #numara ekleme, '49' str olarak ekleme
+numbers.append(59)
 numbers.insert(3, 79) #üçüncü index'in yerine 79 sayısını ekle
-numbers.insert(-2, 88) #son indexin yerine 88 ekle - her bi sayı kendine yer açıyor
-print(numbers)
+numbers.insert(-1,52) #son indexin yerine 88 ekle - her bi sayı kendine yer açıyor
 
 numbers.pop() #append ile eklediğin integer'ı siler.
 numbers.pop(0) #sıfırıncı indexi siler
 numbers.pop(-1) #sondaki indexdeki str/int silinir
+numbers.remove(59)
+print(numbers)
+
+numbers.sort()
+numbers.reverse()
+
+letters.sort()      #harf listesi sayısal büyüklük/sırasal olarak sıralanır
+letters.reverse()   #harf listesi tam tersine çevrilir
+
+
+print(numbers)
+print(letters)
+
+print(len(numbers))      #len - eleman sayısını gösterir
+print(len(letters))
+
+print(numbers.count(10))    #count metodunu çağırıp numbers üzerinden kaç tane 7 var öğreniriz
+print(letters.count('a'))   #kaç tane a var
+
+numbers.clear()    #numbersdaki tüm elemanlı siler []
 print(numbers)
 
 numbers.remove(7) #remove metodu silme istediğin karakteri ver, ,indexi değil
 print(numbers)
-
-numbers.sort() #liste sayısal büyüklük olarak sıralanır
-numbers.reverse() #liste tam tersine çevrilir
-print(numbers)
-letters.sort()
-letters.reverse()
-print(letters)
-
-print(len(numbers)) #eleman sayısını yaz
-print(len(letters))
-
-print(numbers.count(7)) #count metodunu çağırıp numbers üzerinden kaç tane 7 int var öğreniriz
-print(letters.count('a')) #kaç tane a var
-
-numbers.clear() #numbersdaki tüm elemanlı siler []
-print(numbers) 
