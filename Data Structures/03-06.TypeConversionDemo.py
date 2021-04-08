9be380d8cc00dd10db34f72d20c48634727b3411
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-08 22:34:08 +0300

    Update 03-06.TypeConversionDemo.py

--- Data Structures/03-06.TypeConversionDemo.py
+++ Data Structures/03-06.TypeConversionDemo.py
@@ -3,28 +3,24 @@ Dairenin Alanı: πr²
 Dairenin çevresi: 2πr
 
 r = yariCap
-Yarı Çapı verilen bir dairenin alan çapı ve çevresini hesapla 
-(r: 3.14)
+Yarı Çapı verilen bir dairenin alan çapı ve çevresini hesapla  (r: 3.14)
 """
 
 pi = 3.14
 r = float(input("yarı çap: "))
-alan = pi * (r ** 2)
-cevre = 2 * pi * r 
 
-"""
-#bu durumda str bilgi verir 
-# bu yüzden number türlerine (int ya da float) çevir 
-# input başına float ile string floata çevrilir
-"""
+alan = pi * (r ** 2)
+print(type(alan))
+
+cevre = 2 * pi * r
+print(type(cevre))
+
+print("alan: "+ str(alan) + " çevre: "+ str(cevre))    #bu durumda str bilgi verir 
+#print("alan: "+ alan + "çevre: "+ çevre)  #böyle yazılmaz çünkü str bi bilgi gibi, kabul etmez 
+# çünkü alan dediğimiz yer - float  # başına str ekle - float to str işlemi
+
 print("alan:", alan)
 print("cevre:", cevre)
 
 
-"""
-print("alan: "+ alan + "çevre: "+ çevre)
-#böyle yazılmaz çünkü str bi bilgi gibi, kabul etmez 
-# çünkü alan dediğimiz yer - float 
-# başına str ekle - float to str işlemi
-print("alan: "+ str(alan) + "çevre: "+ str(çevre))
-"""
+
