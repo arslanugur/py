8e88d573d1048efdffd9547e282874c8e911260b
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-08 06:38:11 +0300

    Update 03-18.DictionaryDemo.py

--- Data Structures/03-18.DictionaryDemo.py
+++ Data Structures/03-18.DictionaryDemo.py
@@ -1,8 +1,8 @@
 
 students = {
     '120':{
-        'name' : 'Ugur',
-        'surname' : 'Arslan',
+        'name' : 'Amadeus',
+        'surname' : 'Mozart',
         'phone' : '507'
     },
     '125':{
@@ -18,23 +18,30 @@ students = {
 }
 
 #1-bilgileri verilen studentsların kullanıcıdan aldığınız bilgilerle dictionary içinde sakla
-# bi listeyi öğrenciden aldığımız bilgilere göre hazırla
-#kullanıcaya no gir adı dicez sözlük yapısı oluşturup liste üzerine eklicez
-students = {}   #sözlük veri tipi tanımla
+
+#bi listeyi öğrenciden aldığımız bilgilere göre hazırlayıp, 
+#kullanıcaya input vererek sözlük yapısı oluşturup liste üzerine eklicez
+
+students = {}   #sözlük veri tipi tanımlandı
 number = input('student no: ')
 name = input('student name: ')
 surname = input('student surname: ')
 phone = input('student mobile: ')
-# kullanıcadan alacağımız bilgilerle dolacak
+
+#kullanıcadan alacağımız bilgilerle dolacak
 #şuan için döngü yapısını bilmediğimiz için copy paste yapıyoruz yukarıdaki düzeni
+#update in aksine birden fazla veri girmek için copy paste yapman gerek
+
 students[number] = {
     'name': name,
     'surname': surname,
     'phone': phone
 }
+
 # kullanıcan alınan bilgilerle liste oluşturdu
-#update in aksine birden fazla veri girmek için copy paste yapman gerek
-## print(students)
+
+##print(students)
+
 
 # diğer bi şekilde update metodunu da kullanabiliriz
 students.update({
@@ -44,8 +51,10 @@ students.update({
         'phone': phone
     }
 }) #},) diyerek birden fazla veriyi girersin
-#update ile birden fazla veriyi liste üzerine ekleme şansımız var
+
+###update ile birden fazla veriyi liste üzerine ekleme şansımız var
 print(students)
+
 
 #ŞİMDİ LOOP BİLMEDİĞİMİZ İÇİN BUNU ÜÇ KERE TEKRARLIYORUZ AŞAĞIDA
 students.update({
@@ -85,21 +94,21 @@ surname = input('student surname: ')
 phone = input('student mobile: ') 
 
 #print(students)
-# yukarıdaki printi eğer süslersek
+#printi eğer süslersek
 print('*'*50)
+
+
+
 #2-students numaralarını kullanıcıdan alıp ilgili student bilgilerini göster
 
-stuNo = input('student no: ')  #kullanıcıdan öğreni nosunu iste
-#stuNo adında bi değişken içine de atadın
-#bulunan öğrenci öğrenci isminde girilir
+stuNo = input('student no: ')  #kullanıcıdan input iste
+#stuNo adında bi değişken içine de atadın, bulunan öğrenci öğrenci isminde girilir
 student = students[stuNo]
 print(student)
-#yukardaki printteki öğrenciyi obje şeklinde gösteermek yerine f str oluştur
+#yukardaki printteki öğrenciyi obje şeklinde göstermek yerine f str oluştur
 print(f"Aradığınız {stuNo} nolu öğrencinin adı: {student['name']} surname: {student['surname']} ve telefonu ise {student['phone']}")
 
-#yıldızlar sonrası üste doldurduğun verilernden öğrenci numarasıyla f str ile yazılanları çıkaracak
+#yıldızlar sonrası üste doldurduğun verilerinden öğrenci numarasıyla f str ile yazılanları çıkaracak
 
-#bu derste sözlü yapısını kullanıcıdan alınan bilgilere göre oluşturuldu
-#bunu daha sağlıklısı loop yani döngü yapısını bilmek
-# mesela 10 öğrenci ise 10 kere döngü çalışır ve ona göre çıkar
-
+#burada yapılan sözlük yapısını kullanıcıdan alınan bilgilere göre oluşturulması
+#bunu daha sağlıklısı loop yani döngü yapısını bilmek, mesela 10 öğrenci ise 10 kere döngü çalışır ve ona göre çıkar
