408102a862b8d92ffec83da2289a70b8d818c2c4
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-08 22:26:28 +0300

    Update 03-15.ListsMethodsDemo.py

--- Data Structures/03-15.ListsMethodsDemo.py
+++ Data Structures/03-15.ListsMethodsDemo.py
@@ -1,59 +1,59 @@
 names = ['Lincoln', 'Franklin', 'Roosevelt', 'Nixon']
 years = [1933, 1969, 1764, 1861]
 
-# 'Kennedy' ismini liste sonuna ekle
-names.append('Kennedy')
-print(names)
-# 'Washington' listenin başına ekle
-names.insert(0, 'Washington') 
+
+names.append('Kennedy')          #'Kennedy' ismini liste sonuna ekle
+names.insert(0, 'Washington')    #'Washington' listenin başına ekle
 names.insert(len(names), 'Bush') #append gibi değil, index istiyor. ama append gibi sona ekleme de yapılabilir
 print(names) 
-# 'Franklin' ismini listeden sil
-names.remove('Franklin')
-#names.pop() bi indeks vermediğin zaman listenin sonundaki elemanı siler
+
+names.remove('Franklin')         #'Franklin' ismini listeden sil
+names.pop()                      #bi indeks vermediğin zaman listenin sonundaki elemanı siler
 print(names)
-# 'Roosevelt' isminin indexi ne
-index = names.index('Roosevelt')
+
+index = names.index('Roosevelt') #indexini gösterir
 print(index)
 
 names.pop(index)    #aldığın indexe göre elemanı silmek istersn
 print(names)
-# 'Washington' listenin bi elemanı mı
-result = 'Washington' in names #result varsa true false var
+
+result = 'Washington' in names # 'Washington' listenin bi elemanı mı #in names, true false verir
 #diğer yol: result = names.index('Washington') #eğer terminaldeki index numarası -1 değilse true
 print(result)
-# liste elemanlarını ters çevir
-names.reverse()
+
+names.reverse()      #liste elemanlarını ters çevir
 print(names)
-# liste elemanlarını alfabetik olarak sırala
-names.sort()
+
+names.sort()         # liste elemanlarını alfabetik olarak sırala
 print(names)
-# years listesini rakamsal büyüklüe göre sırala
-years.sort()
+
+years.sort()         # years listesini rakamsal büyüklüe göre sırala
 print(years)
-# str = 'Obama, Trump' karakter dizisini listeye çevir
-str = 'Obama, Trump'
+
+str = 'Obama, Trump' # str = 'Obama, Trump' karakter dizisini listeye çevir
 result = str.split(',')
 print(result)
-# years dizisinin en büyük ve en kçük elemanı ne
-min = min(years)
+
+min = min(years)     # years dizisinin en büyük ve en kçük elemanı ne
 max = max(years)
 print(min, max)
-# years dizisinde kaç tane '1764' değeri var
-result = years.count(1764)
+
+result = years.count(1764)   # years dizisinde kaç tane '1764' değeri var
 print(result)
-# years dizisinin tüm elemanlarını sil
-years.clear()
+
+
+years.clear()   # years dizisinin tüm elemanlarını sil
 print(years)
+
+
 # kullanıcıdan aldığın üç tane başkan bilgisini bi listede sakla
-
 presidents = [] #boş bi obje tanımladık
-
 president = input('presidents: ') #input yaz daha sonra bunu president isimli bir değişkene ata
 presidents.append(president) #aldığımız bu bilgiyi dizi üzerine append edelim
 print(president) #kullanıcı input sayesinde ekrana başkanları girecek
 #print(presidents) şeklinde yazsaydık bize liste şeklinde bilgi verir
-#bunu üç kere yap ama döngüleri(loops) kullanmadan yap 3 kere kopyalamak zorunda kal
+
+#bunu üç kere yaparsak amatörce olacak çünkü, döngüleri(loops) kullanmak daha iyi 3 kere kopyalamak zorunda kalmazdık
 president = input('presidents: ') 
 presidents.append(president)
 print(presidents) 
