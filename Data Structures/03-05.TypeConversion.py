f73709ae38dde2a39823639bb8b1a0c83e1603b0
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-09 04:29:06 +0300

    Update 03-05.TypeConversion.py

--- Data Structures/03-05.TypeConversion.py
+++ Data Structures/03-05.TypeConversion.py
@@ -1,31 +1,28 @@
-#veri tipi dönüşümleri
+#Veri tipi dönüşümleri
 """
 x = input('number1: ')
 y = input('number2: ')
 
 total = x + y
 print(total)
-
+"""
 #input komutu yardımıyla kullanıcı veri girer.
 #str işlem. mesela 2 + 3 = 5 değil, 23 gösterir
-#düzelme yolu, x ve y'yi toplamaya sokmadan önce int çevir
-"""
+#Düzelme yolu, x ve y'yi toplamaya sokmadan önce int çevir
 
-"""
 x = input("number1: ")
 y = input("number2: ")
 
 print(type(x))
 print(type(y))
 
-total = int(x) + int(y) #sayıları int çevir ve total değişkeni at
+total = int(x) + int(y)   #sayıları int çevir ve total değişkeni ata
 
 print(total)
-"""
 
+##################################
 
-#other examples
-"""
+#Other examples
 x = 5               #int
 y = 2.5             #float
 name = "Arslan"     #str
@@ -36,62 +33,47 @@ print(type(y))
 print(type(name))
 print(type(isOnline))
 
-"""
+###################################
 
-#TYPE CONVERSIONS
+###  TYPE CONVERSIONS
 
 #int to float
-"""
 x = 5
-x = float(x)  #x'i floata çevirdik
+x = float(x)  #x'i floata çevirdik, int 5 bilgisi artık float 5 olacak
 print(x)
-print(type(x))
-#int 5 bilgisi artık float 5 olacak 
-"""
+print(type(x))   
 
 #float to int
-"""
 y = 2.5
-y = int(y)
+y = int(y)    #sonucu 2.5 değil, 2 çıkar ondalıklı kısım gider
 print(y)
 print(type(y))
-#sonucu 2.5 değil 2 çıkar ondalıklı kısım gider
-"""
 
-"""
+############################
+
 x = 5
 y = 2.5
 
 result = x + y
-print(result)
-print(type(result))
-"""
-
-#eğer bunları string olarak toplarsak
-"""
-x = 5
-y = 2.5
-
+#ya da eğer bunları string olarak toplarsak
 result = str(x) + str(y)
+
 print(result)
 print(type(result))
-"""
+
+############################
 
 #bool to string
-"""
 isOnline = True
+isOnline = str(isOnline)  #bool işlevini kaybeder sadece yazı olarak true olur
 
-isOnline = str(isOnline)
 print(isOnline)
 print(type(isOnline))
-#bool işlevini kaybeder sadece yazı olarak true olur
-"""
 
 #bool to int
-"""
 isOnline = True
-isOnline = int(isOnline)
+isOnline = int(isOnline)   #sonuç 1 çıkacak. bool yani isonline false ise sonuç 0 sıfır
+
 print(isOnline)
 print(type(isOnline))
-#sonuç 1 çıkacak. bool yani isonline false ise sonuç 0 sıfır
-"""
+
