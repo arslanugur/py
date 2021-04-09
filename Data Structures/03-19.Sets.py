020d7909cfb7b942d61c37525e30cc06e721577d
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-09 03:45:40 +0300

    Update 03-19.Sets.py

--- Data Structures/03-19.Sets.py
+++ Data Structures/03-19.Sets.py
@@ -1,37 +1,37 @@
 #set listeleri
 fruits = {'apple', 'orange', 'banana'} #tipik liste örneği
 print(fruits)
-#ama indexlenemez bi liste
-# print(fruits[0]) yapamazsın
-#aşağıdaki gibi elemanlarına döngü ile ulaşırız
-for x in fruits: #her bi elemanı xin yerine sırayla koyalıyoruz
-    print(x)  #sıra sıra çıkar ancak denildiği gibi bunlarda index no yok sıralanmıyor 
 
-#yeni bi eleman ekleme ise
-fruits.add('cherry') #birden fazla olursa aynı işlem tekrar
-#update yöntemiyle listeye yeni bi liste gönderip ekleme yapılır
-fruits.update(['mango', 'grape'])  
-#update'e zaten var olan bi eleman eklemeye kalkarsan göstermez çünkü zaten var
-#kümeler gibi
+#print(fruits[0]) yapamazsın yani indexlenemez bi liste
+#aşağıdaki gibi elemanlarına döngü ile ulaşırız
+
+for x in fruits:    #her bi elemanı x'in yerine sırayla koyarız
+    print(x)        #sıra sıra çıkar ancak denildiği gibi bunlarda index no yok sıralanmıyor 
+
+
+fruits.add('cherry')                 #yeni bi eleman ekleme. Birden fazla olursa aynı işlem tekrar
+fruits.update(['mango', 'grape'])    #update yöntemiyle listeye yeni bi liste gönderip ekleme yapılır
+ 
+#update'e zaten var olan bi eleman eklemeye kalkarsan göstermez çünkü zaten var, kümeler işte
 print(fruits)
-#silme işlemini nasıl yaparız peki
-fruits.remove('mango')
-fruits.discard('orange') #bu da diğer silme yöntemi
+
+fruits.remove('mango')      #silme işlemi
+fruits.discard('orange')    #diğer silme yöntemi
 print(fruits)
-#eğer liste indexlenebiliyorsa pop meteduyla son elemanı siler 
+
+# eğer liste indexlenebiliyorsa pop meteduyla son elemanı siler 
 # ama bu liste için olmaz çünkü sıralı liste değil ve son elemanın silineceğinin garantisi yok 
-fruits.pop() #denendi ilk elemanı sildi
+fruits.pop()       #denendi ilk elemanı sildi
 print(fruits)
 
 fruits.clear() #tüm elemanlar silinir
 print(fruits)  #set()
 
-#yeni bi list tanılarsak
-myList = [1,3,5,5,2,3,1]
-print(myList) #hepsi yazılır
-#bunu set constructer aracılığıyla myListe gönderirsek
-print(set(myList)) #bir kere yazılacak her eleman
+############################
 
+myList = [1,3,5,5,2,3,1]     #yeni bi list tanılarsak
+print(myList)
 
-
+#MyList, set constructer aracılığıyla myListe gönderirsek, her bir eleman bir kere yazılacak
+print(set(myList)) 
 
