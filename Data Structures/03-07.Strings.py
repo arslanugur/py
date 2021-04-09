27a21e7a4932b859e9bbb48e8429b1363375f65f
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-09 03:15:23 +0300

    Update 03-07.Strings.py

--- Data Structures/03-07.Strings.py
+++ Data Structures/03-07.Strings.py
@@ -1,51 +1,48 @@
-#karakter dizileri: string
+#Karakter Dizileri: String
 
 """
-name = "Uğur"
-surname = "Arslan"
+name = "Aretha"
+surname = "Franklin"
 age = 30
  
-print("My name is "+ name + " "+ surname + " and \nI'm "+ str(age) + " y/o.")
+print("Her name is "+ name + " "+ surname + " and \nI'm "+ str(age) + " y/o.")    
 
-#yada deneme = my name is adında string de atayabiliriz
 #str(age) ile tür dönüşümü yaptık
+#deneme = her name is.. adında string de atayabilirdik
+#greating = "Her name is "+ name + " "+ surname + " and \nShe is "+ str(age) + " y/o."
+#print(greeting)
+
 #\n ifadesi ile sonrasında gelen cod bi alt satıra iner
-# ya da greating = "My name is "+ name + " "+ surname + " and \nI'm "+ str(age) + " y/o."
-# sonra print(greeting)
 """
 """
-# eğer ifadelerden birinin nosunu yazdırırsak sıra numarası gelir
-print(name[0]) #ya da print(greeting[2]) gibi
+#eğer ifadelerden birinin nosunu yazdırırsak sıra numarası gelir
+print(name[0]) 
+#ya da
+print(greeting[2])
 """
 
-#print(len(greeting)) greeting ifadesinin kaç karakterli olduğunu söyler
+greeting = 'Her name is '+ name + ' '+ surname + ' and \nShe is '+ str(age) + ' years old'
+print(len(greeting)) #atanmış greeting ifadesinin kaç karakterli olduğunu söyler
 #bunu bu şekilde de yapılabilir length = len(greeting)
-#print(greeting[length-1]) ya da print(greeting[-1])ile de SON karakteri buluruz
-#print(greeting[2:5]) bu durumda 2. indexden başka 5.indexe kadar gider
-#print(greeting[3:]) bu durumda 3den başlar sonuna kadar gider
-#print(greeting[:15]) bu durumda baştan 15e kadar gider
-# print(greeting[2:40:2]) 2den 40a kadar ama her iki karakterde bir alır
+
+#print(greeting[length-1]) #Son karakteri buluruz
+#print(greeting[2:5])      #2.indexden başlar 5.indexe kadar gider
+#print(greeting[3:])       #3.indexden başlar sonuna kadar gider
+#print(greeting[:15])      #Baş karakterden 15e kadar gider
+#print(greeting[2:40:2])   #2den 40a kadar ama her iki karakterde bir alır
   
-#bu yapılanlar string formatlamayla bağlantılı + ile yaptık yani zordu
+#bu yapılanlar string formatlamayla bağlantılı: + ile yaptık
 
 
+###############
 
-#name = 'arslan'
-
+#name = 'OtisRedding'
 # print(dir(name)) #neler yapabilirsin
-
 # print(help(str)) #tanım yapar
-name = 'arslan\'s dog'
-# message = f'hello {name}'
-# print(message)   #ya da
-message = 'hello {}'
-print(message.format(name))#name = 'arslan'
 
-# print(dir(name)) #neler yapabilirsin
-
-# print(help(str)) #tanım yapar
-name = 'arslan\'s dog'
-# message = f'hello {name}'
-# print(message)   #ya da
+name = 'Redding\'s song'
+#message = f'hello {name}'
+#print(message)   
+#ya da
 message = 'hello {}'
 print(message.format(name))
