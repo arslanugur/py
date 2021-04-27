24845468406795f3eb7b1b0b43dbfc2a18971d30
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-27 04:09:33 +0300

    Create Writing Functions in Python.py

---
+++ Writing Functions in Python.py
@@ -0,0 +1,123 @@
+1
+Best Practices
+FREE
+0%
+The goal of this course is to transform you into a Python expert, and so the first chapter starts off with best practices when writing functions. We'll cover docstrings and why they matter and how to know when you need to turn a chunk of code into a function. You will also learn the details of how Python passes arguments to functions, as well as some common gotchas that can cause debugging headaches when calling functions.
+
+Docstrings
+50 XP
+Crafting a docstring
+100 XP
+Retrieving docstrings
+100 XP
+Docstrings to the rescue!
+50 XP
+DRY and "Do One Thing"
+50 XP
+Extract a function
+100 XP
+Split up a function
+100 XP
+Pass by assignment
+50 XP
+Mutable or immutable?
+50 XP
+Best practice for default arguments
+100 XP
+Hide Details
+
+
+2
+Context Managers
+0%
+If you've ever seen the "with" keyword in Python and wondered what its deal was, then this is the chapter for you! Context managers are a convenient way to provide connections in Python and guarantee that those connections get cleaned up when you are done using them. This chapter will show you how to use context managers, as well as how to write your own.
+
+Using context managers
+50 XP
+The number of cats
+100 XP
+The speed of cats
+100 XP
+Writing context managers
+50 XP
+The timer() context manager
+100 XP
+A read-only open() context manager
+100 XP
+Advanced topics
+50 XP
+Context manager use cases
+50 XP
+Scraping the NASDAQ
+100 XP
+Changing the working directory
+100 XP
+Hide Details
+
+
+
+3
+Decorators
+0%
+Decorators are an extremely powerful concept in Python. They allow you to modify the behavior of a function without changing the code of the function itself. This chapter will lay the foundational concepts needed to thoroughly understand decorators (functions as objects, scope, and closures), and give you a good introduction into how decorators are used and defined. This deep dive into Python internals will set you up to be a superstar Pythonista.
+
+Functions are objects
+50 XP
+Building a command line data app
+100 XP
+Reviewing your co-worker's code
+100 XP
+Returning functions for a math game
+100 XP
+Scope
+50 XP
+Understanding scope
+50 XP
+Modifying variables outside local scope
+100 XP
+Closures
+50 XP
+Checking for closure
+100 XP
+Closures keep your values safe
+100 XP
+Decorators
+50 XP
+Using decorator syntax
+100 XP
+Defining a decorator
+100 XP
+Hide Details
+
+
+
+4
+More on Decorators
+0%
+Now that you understand how decorators work under the hood, this chapter gives you a bunch of real-world examples of when and how you would write decorators in your own code. You will also learn advanced decorator concepts like how to preserve the metadata of your decorated functions and how to write decorators that take arguments.
+
+Real-world examples
+50 XP
+Print the return type
+100 XP
+Counter
+100 XP
+Decorators and metadata
+50 XP
+Preserving docstrings when decorating functions
+100 XP
+Measuring decorator overhead
+100 XP
+Decorators that take arguments
+50 XP
+Run_n_times()
+100 XP
+HTML Generator
+100 XP
+Timeout(): a real world example
+50 XP
+Tag your functions
+100 XP
+Check the return type
+100 XP
+Great job!
