5fc9601f3a522c620f44033fdce43650a1488c80
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-29 14:55:49 +0300

    Update 04. Advanced Regular Expression Concepts.py

--- Regular Expressions in Py/04. Advanced Regular Expression Concepts.py
+++ Regular Expressions in Py/04. Advanced Regular Expression Concepts.py
@@ -1,1 +1,18 @@
+4. Advanced Regular Expression Concepts
+You will learn more complex methods of pattern matching using parentheses to group strings together 
+or to match the same text as matched previously. Also, you will get an idea of how you can look around expressions.
 
+4.1. Capturing groups
+4.2. Try another name
+4.3. Flying home
+4.4. Alternation and non-capturing groups
+4.5. Love it!
+4.6. Ugh! Not for me!
+4.7. Backreferences
+4.8. Parsing PDF files
+4.9. Close the tag, please!
+4.10. Reeepeated characters
+4.11. Lookaround
+4.12. Surrounding words
+4.13. Filtering phone numbers
+4.14. Finishing line
