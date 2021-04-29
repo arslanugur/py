28703f5a89388d9b2f0e6041a323c7cfc0515fb1
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-29 14:54:46 +0300

    Update 03. Regular Expressions for Pattern Matching.py

--- Regular Expressions in Py/03. Regular Expressions for Pattern Matching.py
+++ Regular Expressions in Py/03. Regular Expressions for Pattern Matching.py
@@ -1,1 +1,23 @@
+3. Regular Expressions for Pattern Matching
+Time to discover the fundamental concepts of regular expressions! 
+In this key chapter, you will learn to understand the basic concepts of regular expression syntax. 
+Using a real dataset with tweets meant for sentiment analysis, 
+you will learn how to apply pattern matching using normal and special characters, and greedy and lazy quantifiers.
+
+3.1. Introduction to regular expressions
+3.2. Are they bots?
+3.3. Find the numbers
+3.4. Match and split
+3.5. Repetitions
+3.6. Everything clean
+3.7. Some time ago
+3.8. Getting tokens
+3.9. Regex metacharacters
+3.10. Finding files
+3.11. Give me your email
+3.12. Invalid password
+3.13. Greedy vs. non-greedy matching
+3.14. Understanding the difference
+3.15. Greedy matching
+3.16. Lazy approach
 
