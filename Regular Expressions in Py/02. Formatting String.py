8b24b00c9d5b0f917a712e1a4ffd38d9c0c71c36
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-29 14:53:48 +0300

    Update 02. Formatting String.py

--- Regular Expressions in Py/02. Formatting String.py
+++ Regular Expressions in Py/02. Formatting String.py
@@ -1,1 +1,18 @@
+2. Formatting Strings
+You will learn the main approaches that can be used to format 
+or interpolate strings in python using a dataset containing information scraped from the web. 
+You will explore the advantages and disadvantages 
+of using positional formatting, embedding expressing inside string constants, and using the Template class.
 
+2.1. Positional formatting
+2.2. Put it in order!
+2.3. Calling by its name
+2.4. What day is today?
+2.5. Formatted string literal
+2.6. Literally formatting
+2.7. Make this function
+2.8. On time
+2.9. Template method
+2.10. Preparing a report
+2.11. Identifying prices
+2.12. Playing safe
