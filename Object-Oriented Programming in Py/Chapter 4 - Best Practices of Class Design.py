d5d64ef59830695fd601adb3f152d57a0441e760
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-04-29 05:16:21 +0300

    Update Chapter 4 - Best Practices of Class Design.py

--- Object-Oriented Programming in Py/Chapter 4 - Best Practices of Class Design.py
+++ Object-Oriented Programming in Py/Chapter 4 - Best Practices of Class Design.py
@@ -83,24 +83,7 @@ class TalkativeChild(Parent):
 p, c, tc = Parent(), Child(), TalkativeChild()
 
 for obj in (p, c, tc):
-    obj.talk()class Parent:
-    def talk(self):
-        print("Parent talking!")     
-
-class Child(Parent):
-    def talk(self):
-        print("Child talking!")          
-
-class TalkativeChild(Parent):
-    def talk(self):
-        print("TalkativeChild talking!")
-        Parent.talk(self)
-
-
-p, c, tc = Parent(), Child(), TalkativeChild()
-
-for obj in (p, c, tc):
-    obj.talk()
+    obj.talk()       
     
 What is the output of the code above?
 Parent talking!
