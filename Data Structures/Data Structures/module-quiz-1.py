4ba2abd898a05b18897c9000157a2ad6a221d88c
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-12 17:43:12 +0300

    Create module-quiz-1.py

---
+++ Data Structures/Data Structures/module-quiz-1.py
@@ -0,0 +1,12 @@
+# to output the string containing a single quote.
+print('I\'m learning!')
+
+# Which of the following options results in exactly two lines?
+"Hello \n world"
+
+# What is the output of this code?
+print(3 * '7')   # output: 777
+
+# to convert the text to uppercase and output it.
+text = "wall street"
+print(text.upper())
