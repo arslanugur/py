cef944e6013748c0f4b8adeaaf4aeddcaaf56d5a
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-12 16:41:32 +0300

    Create strings.py

---
+++ Data Structures/Data Structures/strings.py
@@ -0,0 +1,24 @@
+# Strings
+# Strings are used to store text, and are one of the simplest and most used data structures.
+# Any text between two single or double quotation marks is a string.
+
+# For example: The variable x now represents a string.
+x = "Hello world"
+print(x)
+
+
+# There are a number of special characters that can be used in strings. \n represents a new line:
+print('One \nTwo \nThree')
+
+
+
+# Similarly, \t represents a tab.
+# If you want to include a single quote in a single-quoted string (or a double quote in a double-quoted string), 
+# you need to escape it using a backslash:
+print('Brian\'s mother: He\'s not an angel. He\'s a very naughty boy!')
+
+# The backslash is also called the escape character.
+
+
+# Special Characters: How many lines does this code output?
+print("1\n2\n345\n6")   #output: 4
