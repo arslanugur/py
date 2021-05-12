e679fb15cd3dce0d9882b821f149bd80d7ce28ef
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-05-12 16:19:26 +0300

    Create data-structures.py

---
+++ Data Structures/Data Structures/data-structures.py
@@ -0,0 +1,26 @@
+Almost every program uses data.
+Organizing, managing and storing data is important as it enables easier access and efficient modifications.
+
+Data Structures allow you to store and manage your data.
+
+Python has a number of built-in data structures:
+- Strings
+- Lists
+- Dictionaries
+- Tuples
+- Sets
+
+During this course, we will learn about these data structures, use them to store data and see their real-life applications.
+
+If you are not familiar with the basics of Python, please complete our Python for Beginners course first.
+
+
+In addition to the built-in data structures, 
+Python allows you to create your own data structures, enabling you to have full control over their functionality.
+
+The most prominent data structures are:
+- Stacks
+- Queues
+- Trees
+- Linked Lists
+These data structures have many uses in popular algorithms. We will learn about them in the coming modules.
