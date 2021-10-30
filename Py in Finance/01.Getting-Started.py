03117582f0ca539f2846030764eb1346fffdc360
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-10-31 02:01:23 +0300

    Create 01.Getting-Started.py

---
+++ Py in Finance/01.Getting-Started.py
@@ -0,0 +1,33 @@
+      # Py can help to easily get financial data, calculate metrics, generate reports, automate various tasks and perform financial analysis. 
+      
+      # What does ROE stand for? --> Return on Equity
+      # Equity represents the value that would be returned to a company's shareholders if all of the assets were liquidated and all of the company's debts were paid off. 
+      # The calculation of equity is a company's total assets minus its total liabilities, and is used in several key financial ratios such as ROE. 
+      # Key points abt Equity based on finance: 
+      # 1. Equity represents the value that would be returned to a company’s shareholders if all of the assets were liquidated and all of the company's debts were paid off. 
+      # 2. We can also think of equity as a degree of residual ownership in a firm or asset after subtracting all debts associated with that asset. 
+      # 3. Equity represents the shareholders’ stake in the company, identified on a company's balance sheet. 
+      # 4. The calculation of equity is a company's total assets minus its total liabilities, and is used in several key financial ratios such as ROE.
+    
+      # The return on equity (ROE) is a measure of the profitability of a business in relation to the equity. 
+      # Because shareholder's equity can be calculated by taking all assets and subtracting all liabilities, ROE can also be thought of as a return on assets minus liabilities. 
+      # ROE measures how many dollars of profit are generated for each dollar of shareholder's equity. 
+      # ROE is a metric of how well the company utilizes its equity to generate profits.
+      
+      # Equity : for example, if you own a car worth $25,000, but you owe $10,000 on that vehicle, the car represents $15,000 equity. 
+      # RoE : an example of an RoE calculation - Car Company had $1,000 in net income last year and shareholder equity worth $5,000. 
+                                                # This means that its return on equity would be: $1,000 (net income) / $5,000 (shareholder equity) = 20%. 
+                                                # In other words, for every dollar of overall assets that Car Company had last year it saw a return of 20 cents.
+          
+       # Several real-world financial problems and perform calculations.
+       #   Here are some of the projects that we will cover:
+       #   - Calculate basic financial metrics,
+       #   - Plot charts based on financial data,
+       #   - Retrieve and analyze stock prices,
+       #   - Perform financial analysis of stocks and portfolios, included stock returns, correlations, risk analysis,
+       #   - Optimize a portfolio using the Monte Carlo Simulation.
+
+        #  As the practice project, we will build a program to analyze the price of Bitcoin!
+        #  The skills learned during this course can be used to tackle many different financial problems, create reports and help in decision making.
+        
+        
