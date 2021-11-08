a7da3489ae4450efbb6459d270b35b0a04e61601
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-09 02:47:10 +0300

    Create 02. Simple Operations.py

---
+++ 01. Basic Concepts/02. Simple Operations.py
@@ -0,0 +1,52 @@
+# Python has the capability of carrying out calculations.
+# Enter a calculation directly into the print statement:
+print(2 + 2)
+print(5 + 4 - 3)
+  # The spaces around the plus and minus signs here are optional (the code would work without them), but they make it easier to read.
+  
+# Numerical Operations
+'''
+Addition (+) : X+Y  # Description: X plus Y 
+  Eg: 2 + 5 = 7 
+  Eg: 2 + 5.0 = 7.0
+Subtraction (-) : X-Y # Description: X minus Y 
+  Eg: 6 - 2 = 4 
+  Eg: 6 - 2.0 = 4.0
+Multiplication (*) : X*Y # Description: X multiplied by Y 
+  Eg: 2 * 5 = 10 
+  Eg: 2 * 5.0 = 10.0
+Division (/) : X/Y # Description: X divided by Y 
+  Eg: 4 / 2 = 2.0     🔴
+  Eg: 4 / 2.0 = 2.0   🔴
+Exponentiation (**) : X**Y # Description: X to the power of Y 
+  Eg: 2 ** 5 = 32 
+  Eg: 2 ** 5.0 = 32.0
+Quotient (//) : X//Y # Description: How many times Y goes into X 
+  Eg: 3 // 2 = 1 
+  Eg: 3.1 // 2.1 = 1.0
+Remainder (%) : X%Y  # Description: the remainder of X / Y 
+  Eg: 3 % 2 = 1 
+  Eg: 3.1 % 2.1 = 1.0 
+'''
+
+# Examples
+print ("2+5")       # output: 2+5 
+print(2+5)          # output: 7 
+print(eval("2+5")) # output: 7
+
+# Examples
+print(1+2+3)        # 6
+print('1+2+3')      # 1+2+3
+print ('1'+'2'+'3') # 123
+
+# Python also carries out multiplication and division, using an asterisk * to indicate multiplication and a forward slash / to indicate division.
+# Use parentheses to determine which operations are performed first.
+print( 2 * (3 + 4) ) # output: 14
+print( 10 / 2 )      # output: 5.0
+print((4 + 8) / 2)   # output: 6.0
+    # Using a single slash to divide numbers produces a decimal (or float, as it is called in programming). We'll have more about floats
+    # 10/2 gives 5.0 
+    # 10//2 gives 5. double slash cancels decimal/float
+
+
+
