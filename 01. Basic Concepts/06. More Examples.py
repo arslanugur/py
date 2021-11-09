f218f43cd02b4f0e6b334d9d175286764d51b361
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-09 23:00:45 +0300

    Create 06. More Examples.py

---
+++ 01. Basic Concepts/06. More Examples.py
@@ -0,0 +1,44 @@
+print(1 + 4*3)  # We have to solve this problem by BODMAS rule 
+                # First we have to take 4*3 = 4×3 = 12 
+                # Then we have to add 1 with 12 = 1+12 = 13
+
+                # Python is based on operator precedence
+                # It can be anything like arithmetic, logical, relational bitwise etc
+
+print((3**2)//2)    # 4
+                    # 3 ** 2 = 3² 
+                    #          3² = 3 × 3 = 9 
+                    #                       9 // 2
+                    # How many 2's can fit in 9?
+                    # 2 + 2 + 2 + 2 + 1 = 9 
+                    # 4 times 2             9 // 2 = 4
+                    #                       9 / 2 = 4.5 
+
+# The "%" operator takes the remaining amount of a division.
+# 13 % 4 = 1, 4 goes three times in 13 and then 1 is left over.
+
+
+# Example:
+# You are offered a choice of either $1.000.000 or $0.01 (one penny) doubled every day for 30 days (the resulting amount is doubled every day). 
+# Task: Write a program to calculate the amount that will result from the doubling to understand which choice results in a larger amount.
+print (0.01*(2**30))
+
+print(100//42) # 2
+print(20/7)    # 2.8571
+print(20//7)   # 2
+
+
+# Example: to produce code that output 10 raised to the 5th power
+print(10**5)
+
+# pow Examples:
+# pow(x,y)  # x - the figure which is raised. 
+            # y - the figure which raises. 
+            # Their outputs are equal to each other. 
+print(pow(10,5))    # 100000 
+print(10 ** 2)      # 100
+print(pow(9,1/2))   # 3.0 
+print(9 ** (1/2))   # 3.0
+
+
+
