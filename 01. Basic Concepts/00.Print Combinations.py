5bb4b90aefc63a0da9a59d07f090b5e16fe28575
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-09 21:01:18 +0300

    Create 00.Print Combinations.py

---
+++ 01. Basic Concepts/00.Print Combinations.py
@@ -0,0 +1,53 @@
+print("Python takes practice,you'll eventually be good at it")
+"This is called a string,but it will be ignored by python because I didn't use 'print'"
+print("This is a string")
+#Do you see what i'm saying?
+
+#Python also does calculations
+print ("2+(3*2)")
+print ("10/2")    #The code won't work because of the quotation marks
+print (2+(3*2))
+print (10/2)
+print (1/3)
+
+print (2+5.3)     #Note,it becomes 7.3 because 5.3 is a float.
+# Float + Integer = Float
+# Integer + Integer = Integer
+
+print (3*4.5)
+
+#Exponentation (1/2) is square root of 2 and (1/3) root of 3
+print (36**(1/2))
+print (27**(1/3))
+
+#Quotntient/Floor divisions&Remainders
+print (50//6) #This output the amount of 6 you get in 50.
+print (50%6) #This output the remainder after dividing 50 by 6.
+
+print ("""Hello,Tom
+How are you doing today
+Fine.You?
+Not fine,I need a new laptop""")  #This is a multiline
+print ("Hello,Tom\n How are you doing today\nFine.you?\nNot fine,I need a new laptop")
+print ("The time is","5am")
+print ("The time is"+"5am")
+
+#variables
+tom=7
+print (tom*10)
+x=12
+print(x/2)
+x="This is a boy"
+print(x+'3')
+print(x*3)
+
+animals="Dogs"
+food="bones"
+print ("I think"+ animals+ "like"+ food+"!")
+print ("I think {0} like {1}!".format 
+("Dogs","bones"))
+
+name= (input("Enter your name:" ))
+print ("I am " +name+"!")
+age=int (input("How old are you?"))
+print ("I am " +str(age)+ "years old")
