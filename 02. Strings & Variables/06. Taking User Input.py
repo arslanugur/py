8fee9791e6fe0d909e7b303046af1c74d09c4167
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-11 17:40:03 +0300

    Create 06. Taking User Input.py

---
+++ 02. Strings & Variables/06. Taking User Input.py
@@ -0,0 +1,35 @@
+# To get input from the user in Python, you can use the intuitively named input function.
+# For example, a game can ask for the user's name and age as input and use them in the game. 
+
+# The input function prompts the user for input, and returns what they enter as a string (with the contents automatically escaped). 
+x = input()
+print(x)
+
+# Always keep in mind that anything return from input() function is STRING 
+# so if you know that you are asking for number and need to treat the input as integer or float, 
+# just parse, convert the type in programming, function as follows: 
+integer_input = int(input()) 
+float_input=float(input())
+
+# Example: to take user input, assign it to a variable, and output it:
+text = input()
+print(text)
+
+# INPUT
+  # The input statement needs to be followed by parentheses.
+  # You can provide a string to input() between the parentheses, producing a prompt message.
+  # Example
+name = input("Enter your name: ")
+print("Hello, " + name)   
+
+
+
+# Even if the user enters a number as input, it is processed as a string.
+name = (input("Enter your name: ")) 
+print("I'm " +name+"! ") 
+age = int(input("what's your age? ")) 
+print("I'm " + int(age) + " years old ")
+
+# The prompt message helps to clarify what input the program is asking for.
+
+
