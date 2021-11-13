de1a3a041c1f02f48fa68ab769e8189f9caf7380
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-13 21:13:16 +0300

    Create 09. Beginner Set.py

---
+++ 02. Strings & Variables/09. Beginner Set.py
@@ -0,0 +1,71 @@
+# Basic Concepts of Python to run the code you have to enter your name age and again your name 
+
+print('Hello world!') # Basic Python
+
+print(2 + 3) # Addition
+
+print(10 - 5) # Subtraction
+
+print(3 * 5) # Multiplication
+
+print(10 / 2) # Division
+
+print(10 / 3) # Created a float by dividing 10 and 3
+
+print(10 + 1.2) # Adding 10 to 1.2 which is a "float"
+
+print(9**2) # Exponetiation
+
+print(10 // 2) # Quotient 
+
+print(20 % 3) # Remainder
+
+print("we are learning python") #string
+
+print("jolly joker\'s card") #we use the backslash (\) in this sentence
+
+print("one \ntwo \nthree") #we use \n to represent a new line 
+
+print("""we
+are
+learning
+python""") #we can even use the triple quotation to represent words in differnt lines 
+
+print("we \tare \tcoders") #the \t is used to create a space between strings
+
+print(" we " + " are " + " coders ") #concatination is when we add 2 or more strings
+
+print("2" + "3") #we can even convert integers into strings by putting them into double quotation
+
+print(" python " * 3) #we can even multiply strings by integers
+
+x = 10
+print(10) #variables are represented by using the = sign
+    
+print(x + 5) #variables can also be added
+    
+python = 14
+aarav = 15
+del coders
+print(python) #we use the (del) statement to delete something if we print(python + coders) it would have shown an error as aarav has been deleted 
+
+name = input("Enter your name: ")
+print(name) #to ask the user something, we use the input statement
+
+age = int(input())
+print(age) #we use the int statement to covert a string into a integer
+
+name = input()
+print(" hello " + name) #we can even add a string and concatinate it with the input
+
+x = 5
+x += 10
+print(x) #in place operators are used to simplify the code 
+
+x = "coders"
+x += " python "
+print(x) #in palce operators can also be used to add strings
+
+
+
+
