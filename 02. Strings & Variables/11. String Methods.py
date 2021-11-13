a09fd13ca8bb65a498687a340238dc2c2ff776e2
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-14 00:09:26 +0300

    Create 11. String Methods.py

---
+++ 02. Strings & Variables/11. String Methods.py
@@ -0,0 +1,17 @@
+INPUT               METHOD                  OUTPUT
+--------------------------------------------------------------
+"Hello World"       .endswith("by")         False
+"Hello World"       .startswith('Hello')    True
+"hello world"       .capitalize()            "Hello World"
+"13/11/2021"        .split("/")             ["13","11","2021"]
+" Hello "           .strip()                "Hello"
+"Hello A"           .replace("A","B")       "Hello B"
+"hello world"       .count("o")             2
+"Hello World"       .find("o")              4                 # Index Number
+"123456"            .isnumeric()            True
+"HELLO"             .lower()                "hello"
+"hello"             .upper()                "HELLO"
+
+
+
+
