f464af86e47eb8a25332694dfaa6d8f027434284
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-14 02:47:28 +0300

    Update and rename Data Structures/03-04.VariablesDemo.py to 02. Strings & Variables/12. Demo: Variables.py

---
+++ 02. Strings & Variables/12. Demo: Variables.py
@@ -0,0 +1,36 @@
+  # 1- Bir müşterinin aşağıdaki bilgileri için değişken oluşturunuz.
+# Customer name
+# Customer surname
+# Customer name + surname
+# Customer sex
+# Customer identity no
+# Customer birth year
+# Customer address info   
+# Customer age 
+
+customerName = 'James'
+customerSurname = 'Joyce'
+customerNameSurname = customerName + ' ' + customerSurname
+print(customerNameSurname)
+customerSex = True      #Male
+customerIdentityNo = '13165465445'      # to assign as string
+customerBirthyear = 1989
+customerAddress = 'Istanbul'
+customerAge = 2021 - customerBirthyear 
+
+
+  # 2- Aşağıdaki siparişlerin toplam bilgisini hesaplayınız.   
+# Order 1 => 110    TL
+# Order 2 => 1100.5 TL
+# Order 3 => 356.95 TL
+
+# to calculate order information
+order1 = 110
+order2 = 1100.5
+order3 = 356.95
+
+total = order1 + order2 + order3
+
+print("Total:", total)     # or print(order1 + order2 + order3)
+
+
