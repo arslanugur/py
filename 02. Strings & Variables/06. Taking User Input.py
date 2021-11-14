f8578a0c14d8092d9e7f98b518ca6b31ab4539cf
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-14 11:49:36 +0300

    Update 06. Taking User Input.py

--- 02. Strings & Variables/06. Taking User Input.py
+++ 02. Strings & Variables/06. Taking User Input.py
@@ -33,3 +33,15 @@ print("I'm " + int(age) + " years old ")
 # The prompt message helps to clarify what input the program is asking for.
 
 
+# Example:
+# Write a program to take two integers as input and output their sum.
+# Sample Input: 2, 8
+# Sample Output: 10
+
+num1 = int(input())
+num2 = int(input())
+
+print(num1 + num2)
+
+
+
