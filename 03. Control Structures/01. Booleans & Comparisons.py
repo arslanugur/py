7feaaec038868db2e24e571862a30ac0b38ce153
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-16 00:11:32 +0300

    Update 01. Booleans & Comparisons.py

--- 03. Control Structures/01. Booleans & Comparisons.py
+++ 03. Control Structures/01. Booleans & Comparisons.py
@@ -57,8 +57,8 @@ print(2 != 10)                # True
 # You can compare strings, floats, and integers to each other. 
 # A string compared to a float or integer will always show as Not Equal, 
 # but a float and integer with the same value will show as Equal: 
-"2" != 2 True 
-3.0 != 3 False 
+print("2" != 2)  # True 
+print(3.0 != 3)  # False 
     
 # Example:
 # == and is comparison in python explained
@@ -80,17 +80,88 @@ print(7 != 8)         # False
 print(7 > 5)    # True
 print(10 < 10)  # False
   # Different numeric types can also be compared, for example, integer and float. 
-  
+  #(> means greater than) 
+  #(< means less than)   
   
 a= 1==1   # True
 b= 2!=3   # False
 c= 7>6    # True
-x= a+b+c  # T + F + T
+x= a + b + c  # T + F + T
 print(x)  # 3
-  
-  
-  
 
+print(2==2.0000000000000001)  # True 
+print(2==2.000000000000001)   # False
+print(2==float(2.0000000000000001)) #False
+
+print( 7 > 7.0 )  # False --> 7 and 7.0 are the same
+print(7.0 == 7)   # True  --> Only difference is the type 7 is an integer 7.0 is a float
+
+print(1*0.1==0.1)   # True 
+print(2*0.1==0.2)   # True
+print(3*0.1==0.3)   # False 
+print(4*0.1==0.4)   # True
+print(5*0.1==0.5)   # True
+print(6*0.1==0.6)   # False
+print(7*0.1==0.7)   # False
+print(8*0.1==0.8)   # True
+print(9*0.1==0.9)   # True
+print(10*0.1==0.10) # False
+
+
+  # The greater than or equal to, and smaller than or equal to operators are >= and <=.
+  # They are the same as the strict greater than and smaller than operators, except that they return True when comparing equal numbers. 
+print(7 <= 8)   # True
+print(9 >= 9.0) # True
+
+  # Greater than and smaller than operators can also be used to compare strings lexicographically 
+  # (the alphabetical order of words is based on the alphabetical order of their component letters).
+  # For example:
+print("Annie" > "Andy") # True
+
+  # The first two characters from "Annie" and "Andy" (A and A) are compared. 
+  # As they are equal, the second two characters are compared. 
+  # Because they are also equal, the third two characters (n and d) are compared. 
+  # And because n has greater alphabetical order value than d, "Annie" is greater than "Andy".
   
+# Conclusion:
+# == equals logics 2==3 gives false as 2 is different from 3 
+# != equals illogics 2 != 3 gives true 
+# >= / > / <= / < indicates something larger, smaller or equals something else python compares integers and strings too. 
+# "too">"to" is true as 3 letters is more than 2 letters. 
+# "sea"<"tea" is true as t comes after s in the alphabetical order. Lexicographically a < d = True, g > m = False
+# print(9.0000000000000001 == 9.0) True
+
+# Example:
+a = 4 == 4 
+b = 3 != 3 
+c = 8 < 6 
+d = 5<= 7 
+print("a =", a, "\nb =", b, "\nc =", c, "\nd =", d, "\nSum =", a + b + c + d) 
+# Output:
+# a = True 
+# b = False 
+# c = False 
+# d = True 
+# Sum = 2
+
+# Example:
+print( 8.7 <= 8.70 )  # True
+
+
+# 1.Booleans there are two types a.True b.False 
+# 2.Comparison Operators 
+  # a)(== #this is used to compare the values of both the side ) eg: 2==2.0 which is True & 3==2 which is False 
+  # b)(!= #this implies that the values on both sides are not equal ) eg: 2!=3 which is true & 3!=3 which is False 
+  # c)( >< #they say that the value in one side is greater than the other or vice-versa) eg :2<3 which is true & 2>3 which is False 
+  # d)(>= ,<= # they are same like the last on except in the fact that they turn true when the values in the both sides are equal) 
+       # eg:3>=2 which is True 3<=3 which is also True
+                                                                                                        
+# The value of alphabets increases as we go from 'a' to 'z'. 
+# Every capital alphabet also follow this rule and apart this all capital alphabets have lesser value than 'a'. 
+print('a' >'b' )        # False 
+print('A' < 'a')        # True
+print('a' > 'B')        # True                                                                                                       
+print("Love" > "Life")  # True  --> it is because "o" comes after "i" . 
+
 
 
