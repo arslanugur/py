8c4c321062893084c3349d8d6461af3b9a022bd0
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-16 17:58:47 +0300

    Create 03. else Statements.py

---
+++ 03. Control Structures/03. else Statements.py
@@ -0,0 +1,33 @@
+# The if statement allows you to check a condition and run some statements, if the condition is True.
+# The else statement can be used to run some statements when the condition of the if statement is False.
+
+# As with if statements, the code inside the block should be indented.
+x = 4
+if x == 5:
+    print("Yes")
+else:             # Notice the colon after the else keyword.
+    print("No")
+    
+# Example:
+x = int(input("Enter a number: "))
+if x == 5:
+    print("Yes")
+else: 
+    if x < 5: 
+        print("Less than 5") 
+    else: 
+        if x > 5: 
+            print("Greater than 5")
+            
+# Example:
+n= int(input("Enter a number"))
+if (n>0):
+  print("positive")
+elif (n<0): 
+  print("negative") 
+else: 
+  print("zero")
+  
+# Example:
+
+              
