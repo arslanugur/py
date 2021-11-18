2383026f4f3457a36d003e16965c629e8ccb2c3a
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-18 23:18:30 +0300

    Update 05. Demo: Boolean Age Groups.py

--- 03. Control Structures/05. Demo: Boolean Age Groups.py
+++ 03. Control Structures/05. Demo: Boolean Age Groups.py
@@ -1,4 +1,4 @@
-# Task 
+# Task: 
 # Given the age of a person as input, you need to output their age group. 
 # Here are the age groups you need to handle: 
 # Child: 0 to 11 
@@ -6,7 +6,7 @@
 # Adult: 18 to 64 
 # Senior: 65+ 
 
-# Solution
+# Solution:
 age = int(input())
 if (age >= 0) and (age <= 11): 
   print("Child") 
