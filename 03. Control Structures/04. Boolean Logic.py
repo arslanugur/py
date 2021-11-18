82cf607bd234eace88b7cc80d850cd786b6d6e53
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-18 10:37:33 +0300

    Create 04. Boolean Logic.py

---
+++ 03. Control Structures/04. Boolean Logic.py
@@ -0,0 +1,10 @@
+# Boolean logic is used to make more complicated conditions for if statements that rely on more than one condition.
+# Python's Boolean operators are and, or, not.
+# The and operator takes two arguments, and evaluates as True if, and only if, both of its arguments are True. Otherwise, it evaluates to False. 
+print(1 == 1 and 2 == 2)  # True
+print(1 == 1 and 2 == 3)  # False
+print(1 != 1 and 2 == 2)  # False
+print(2 < 1 and 3 > 6)    # False
+
+# Boolean operators can be used in expression as many times as needed.
+
