fe0d59daa5915e77229acb96a4565005287cfb20
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-19 18:48:41 +0300

    Update 08. Demo: Fruit Vending Machine.py

--- 03. Control Structures/08. Demo: Fruit Vending Machine.py
+++ 03. Control Structures/08. Demo: Fruit Vending Machine.py
@@ -16,4 +16,28 @@ else:
   print("Wrong number")
 
   
+# Second Way:  
+fruits = ["apple", "cherry", "banana", "kiwi", "lemon", "pear", "peach", "avocado"] 
+num = int(input()) #your code goes here 
+
+if num == 0: 
+  print(fruits[0]) 
+elif num == 1: 
+  print(fruits[1]) 
+elif num == 2: 
+  print(fruits[2]) 
+elif num == 3: 
+  print(fruits[3]) 
+elif num == 4: 
+  print(fruits[4]) 
+elif num == 5: 
+  print(fruits[5]) 
+elif num == 6: 
+  print(fruits[6]) 
+elif num == 7: 
+  print(fruits[7]) 
+else: 
+  print("Wrong number")
+
+  
   
