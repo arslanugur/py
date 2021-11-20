e04fd4c9f42f43099e0d0c23acd05cc8c55356f7
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-20 04:36:22 +0300

    Create 20. Demo: Tic Tac Toe.py

---
+++ 03. Control Structures/20. Demo: Tic Tac Toe.py
@@ -0,0 +1,159 @@
+print('enter the line')
+d=int(input())
+print('enter a column')
+b=int(input())
+print ('enter x')
+y=[['֎','֎','֎'],
+   ['֎','֎','֎'],
+   ['֎','֎','֎']]
+y[d][b]=str(input())
+print(y[0])
+print(y[1])
+print(y[2])
+if y[0][0]==y[0][1]==y[0][2]=='x' or y[1][0]==y[1][1]==y[1][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][0]==y[2][0]=='x' or y[0][1]==y[1][1]==y[2][1]=='x' or y[0][2]==y[1][2]==y[2][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][1]==y[2][2]=='x' or y[0][2]==y[1][1]==y[2][0]=='x' or y[0][0]==y[0][1]==y[0][2]=='0' or y[1][0]==y[1][1]==y[1][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][0]==y[2][0]=='0' or y[0][1]==y[1][1]==y[2][1]=='0' or y[0][2]==y[1][2]==y[2][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][1]==y[2][2]=='0' or y[0][2]==y[1][1]==y[2][0]=='0' :
+    print('congratulations on the victory!')
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    y=0
+else:
+    print('enter the line')
+    d=int(input())
+    print('enter a column')
+    b=int(input())
+    print ('enter 0')
+    y[d][b]=str(input())
+    print(y[0])
+    print(y[1])
+    print(y[2])
+if y[0][0]==y[0][1]==y[0][2]=='x' or y[1][0]==y[1][1]==y[1][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][0]==y[2][0]=='x' or y[0][1]==y[1][1]==y[2][1]=='x' or y[0][2]==y[1][2]==y[2][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][1]==y[2][2]=='x' or y[0][2]==y[1][1]==y[2][0]=='x' or y[0][0]==y[0][1]==y[0][2]=='0' or y[1][0]==y[1][1]==y[1][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][0]==y[2][0]=='0' or y[0][1]==y[1][1]==y[2][1]=='0' or y[0][2]==y[1][2]==y[2][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][1]==y[2][2]=='0' or y[0][2]==y[1][1]==y[2][0]=='0' :
+    print('congratulations on the victory!')
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    y=0
+else:
+    print('enter the line')
+    d=int(input())
+    print('enter a column')
+    b=int(input())
+    print ('enter x')
+    y[d][b]=str(input())
+    print(y[0])
+    print(y[1])
+    print(y[2])
+if y[0][0]==y[0][1]==y[0][2]=='x' or y[1][0]==y[1][1]==y[1][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][0]==y[2][0]=='x' or y[0][1]==y[1][1]==y[2][1]=='x' or y[0][2]==y[1][2]==y[2][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][1]==y[2][2]=='x' or y[0][2]==y[1][1]==y[2][0]=='x' or y[0][0]==y[0][1]==y[0][2]=='0' or y[1][0]==y[1][1]==y[1][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][0]==y[2][0]=='0' or y[0][1]==y[1][1]==y[2][1]=='0' or y[0][2]==y[1][2]==y[2][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][1]==y[2][2]=='0' or y[0][2]==y[1][1]==y[2][0]=='0' :
+    print('congratulations on the victory!')
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    y=0
+else:
+    print('enter the line')
+    d=int(input())
+    print('enter a column')
+    b=int(input())
+    print ('enter 0')
+    y[d][b]=str(input())
+    print(y[0])
+    print(y[1])
+    print(y[2])
+if y[0][0]==y[0][1]==y[0][2]=='x' or y[1][0]==y[1][1]==y[1][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][0]==y[2][0]=='x' or y[0][1]==y[1][1]==y[2][1]=='x' or y[0][2]==y[1][2]==y[2][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][1]==y[2][2]=='x' or y[0][2]==y[1][1]==y[2][0]=='x' or y[0][0]==y[0][1]==y[0][2]=='0' or y[1][0]==y[1][1]==y[1][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][0]==y[2][0]=='0' or y[0][1]==y[1][1]==y[2][1]=='0' or y[0][2]==y[1][2]==y[2][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][1]==y[2][2]=='0' or y[0][2]==y[1][1]==y[2][0]=='0' :
+    print('congratulations on the victory!')
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    y=0
+else:
+    print('enter the line')
+    d=int(input())
+    print('enter a column')
+    b=int(input())
+    print ('enter x')
+    y[d][b]=str(input())
+    print(y[0])
+    print(y[1])
+    print(y[2])
+if y[0][0]==y[0][1]==y[0][2]=='x' or y[1][0]==y[1][1]==y[1][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][0]==y[2][0]=='x' or y[0][1]==y[1][1]==y[2][1]=='x' or y[0][2]==y[1][2]==y[2][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][1]==y[2][2]=='x' or y[0][2]==y[1][1]==y[2][0]=='x' or y[0][0]==y[0][1]==y[0][2]=='0' or y[1][0]==y[1][1]==y[1][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][0]==y[2][0]=='0' or y[0][1]==y[1][1]==y[2][1]=='0' or y[0][2]==y[1][2]==y[2][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][1]==y[2][2]=='0' or y[0][2]==y[1][1]==y[2][0]=='0' :
+    print('congratulations on the victory!')
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    y=0
+else:
+    print('enter the line')
+    d=int(input())
+    print('enter a column')
+    b=int(input())
+    print ('enter 0')
+    y[d][b]=str(input())
+    print(y[0])
+    print(y[1])
+    print(y[2])
+if y[0][0]==y[0][1]==y[0][2]=='x' or y[1][0]==y[1][1]==y[1][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][0]==y[2][0]=='x' or y[0][1]==y[1][1]==y[2][1]=='x' or y[0][2]==y[1][2]==y[2][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][1]==y[2][2]=='x' or y[0][2]==y[1][1]==y[2][0]=='x' or y[0][0]==y[0][1]==y[0][2]=='0' or y[1][0]==y[1][1]==y[1][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][0]==y[2][0]=='0' or y[0][1]==y[1][1]==y[2][1]=='0' or y[0][2]==y[1][2]==y[2][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][1]==y[2][2]=='0' or y[0][2]==y[1][1]==y[2][0]=='0' :
+    print('congratulations on the victory!')
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    y=0
+else:
+    print('enter the line')
+    d=int(input())
+    print('enter a column')
+    b=int(input())
+    print ('enter x')
+    y[d][b]=str(input())
+    print(y[0])
+    print(y[1])
+    print(y[2])
+if y[0][0]==y[0][1]==y[0][2]=='x' or y[1][0]==y[1][1]==y[1][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][0]==y[2][0]=='x' or y[0][1]==y[1][1]==y[2][1]=='x' or y[0][2]==y[1][2]==y[2][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][1]==y[2][2]=='x' or y[0][2]==y[1][1]==y[2][0]=='x' or y[0][0]==y[0][1]==y[0][2]=='0' or y[1][0]==y[1][1]==y[1][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][0]==y[2][0]=='0' or y[0][1]==y[1][1]==y[2][1]=='0' or y[0][2]==y[1][2]==y[2][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][1]==y[2][2]=='0' or y[0][2]==y[1][1]==y[2][0]=='0' :
+    print('congratulations on the victory!')
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    y=0
+else:
+    print('enter the line')
+    d=int(input())
+    print('enter a column')
+    b=int(input())
+    print ('enter 0')
+    y[d][b]=str(input())
+    print(y[0])
+    print(y[1])
+    print(y[2])
+if y[0][0]==y[0][1]==y[0][2]=='x' or y[1][0]==y[1][1]==y[1][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][0]==y[2][0]=='x' or y[0][1]==y[1][1]==y[2][1]=='x' or y[0][2]==y[1][2]==y[2][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][1]==y[2][2]=='x' or y[0][2]==y[1][1]==y[2][0]=='x' or y[0][0]==y[0][1]==y[0][2]=='0' or y[1][0]==y[1][1]==y[1][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][0]==y[2][0]=='0' or y[0][1]==y[1][1]==y[2][1]=='0' or y[0][2]==y[1][2]==y[2][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][1]==y[2][2]=='0' or y[0][2]==y[1][1]==y[2][0]=='0' :
+    print('congratulations on the victory!')
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    y=0
+else:
+    print('enter the line')
+    d=int(input())
+    print('enter a column')
+    b=int(input())
+    print ('enter 0')
+    y[d][b]=str(input())
+    print(y[0])
+    print(y[1])
+    print(y[2])
+if y[0][0]==y[0][1]==y[0][2]=='x' or y[1][0]==y[1][1]==y[1][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][0]==y[2][0]=='x' or y[0][1]==y[1][1]==y[2][1]=='x' or y[0][2]==y[1][2]==y[2][2]=='x' or y[2][0]==y[2][1]==y[2][2]=='x' or y[0][0]==y[1][1]==y[2][2]=='x' or y[0][2]==y[1][1]==y[2][0]=='x' or y[0][0]==y[0][1]==y[0][2]=='0' or y[1][0]==y[1][1]==y[1][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][0]==y[2][0]=='0' or y[0][1]==y[1][1]==y[2][1]=='0' or y[0][2]==y[1][2]==y[2][2]=='0' or y[2][0]==y[2][1]==y[2][2]=='0' or y[0][0]==y[1][1]==y[2][2]=='0' or y[0][2]==y[1][1]==y[2][0]=='0' :
+    print('congratulations on the victory!')
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    y=0
+else:
+    print('enter the line')
+    d=int(input())
+    print('enter a column')
+    b=int(input())
+    print ('enter x')
+    y[d][b]=str(input())
+    print(y[0])
+    print(y[1])
+    print(y[2])
+    
+    
+    
