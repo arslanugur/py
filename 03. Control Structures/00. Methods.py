5db78891e148738f86684ca07f946ab853bbe102
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-23 00:04:20 +0300

    Create 00. Methods.py

---
+++ 03. Control Structures/00. Methods.py
@@ -0,0 +1,16 @@
+SET                   LIST              DICTIONARY
+---------------------------------------------------
+add()                 append()          copy()
+clear()               copy()            clear()
+pop()                 count()           fromkeys()
+union()               insert()          items()
+issuperset()          reverse()         get()
+issubset()            remove()          keys()
+intersection()        sort()            pop()
+difference()          pop()             values()
+isdisjoint()          extend()          update()
+setdiscard()          index()           setdefault()
+copy()                clear()           popitem()
+
+
+
