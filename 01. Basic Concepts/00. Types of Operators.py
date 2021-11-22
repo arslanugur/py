cedb1c9ad993a7cfe407d73c4fc45c19b9b863bf
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-22 23:44:48 +0300

    Create 00. Types of Operators.py

---
+++ 01. Basic Concepts/00. Types of Operators.py
@@ -0,0 +1,55 @@
+- Arithmetic Operators
+  +       addition
+  -       subtraction
+  *       multiplication
+  /       division
+  %       modulus
+  **      exponentiation
+  //      floor division
+  
+- Assignment Operators
+  =
+  +=
+  -=
+  *=
+  /=
+  %=
+  //=
+  **=
+  &=
+  |=
+  ^=
+  >>=
+  <<=
+  
+- Comparison Operators
+  ==      equal
+  !=      not equal
+  >       greater than
+  <       less than
+  >=      greater than or equal to
+  <=      less than or equal to
+  
+- Logical Operators
+  and
+  or
+  not
+  
+- Identity Operators
+  is
+  is not
+  
+- Membership Operators
+  in
+  not in
+  
+- Bitwise Operators
+  &       and
+  |       or
+  ^       xor
+  ~       not
+  <<      zero fill left shift
+  >>      signed right shift
+  
+  
+  
