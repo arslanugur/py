17fdbc6c5d357dc472926e583f261f267437940c
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-27 22:20:16 +0300

    Update 18. for Loops.py

--- 03. Control Structures/18. for Loops.py
+++ 03. Control Structures/18. for Loops.py
@@ -1,13 +1,13 @@
 # The for loop is used to iterate over a given sequence, such as lists or strings.
 # The code below outputs each item in the list and adds an exclamation mark at the end:
-words = ["hello", "world", "spam", "eggs"]
-for word in words:
-    print(word + "!")
+movies = ["Interstellar", "Inception", "Prestige", "Tenet"]
+for movie in movies:
+    print(movie + " by Nolan!")
 # output:
-hello!
-world!
-spam!
-eggs!
+Interstellar by Nolan!
+Inception by Nolan!
+Prestige by Nolan!
+Tenet by Nolan!
 
 # In the code above, the word variable represents the corresponding item of the list in each iteration of the loop.
 # During the 1st iteration, word is equal to "hello", and during the 2nd iteration it's equal to "world", and so on.
@@ -65,9 +65,104 @@ print(count) # 2
 # Example:
 list = [2, 3, 4, 5, 6, 7]
 for x in list:
-    if(x%2==1 and x>4):
+    if(x%2==1 and x>4):     # This statement means if the variable x modulus 2 is 1 and variable x is greater than 4 
         print(x)
         break
 # output: 5
 
+# Explaination:
+# x%2 means that after x is divided by 2 there is a remainder of 1. 
+# the "AND" in the if condition means that both parts have to be satisfied, 
+# so we can automatically exclude 2, 3, and 4 since they are <= 4 not >4 That leaves 5, 6, and 7 
+# Since the for loop works from left to right in the list, let's start with 5 5/2 is 2 remainder 1, 
+# so 5%2 =1 and 5>4 so it satisfies the if condition, so the answer is 5! 
+# Because of the 'break', even though 7 also fits the if condition, it will not be printed.
 
+
+
+# FOR versus WHILE
+# Both, for and while loops can be used to execute a block of code for multiple times.
+# Loops: The construct can be used to iterate through a list
+
+# It is common to use the for loop when the number of iterations is fixed. 
+# For example, iterating over a fixed list of items in a shopping list.
+
+# The while loop is used in cases when the number of iterations is not known and depends on some calculations and conditions in the code block of the loop.
+# For example, ending the loop when the user enters a specific input in a calculator program. 
+
+# Both, for and while loops can be used to achieve the same results, 
+# however, the for loop has cleaner and shorter syntax, making it a better choice in most cases.
+
+# Example:
+words=["Hello","world","spam","eggs"] 
+a=0 
+b=len(words)-1  # 4 - 1 = 3 
+while a<=b: 
+    c=words[a] 
+    print(c+"!") 
+    a=a+1
+
+# Second Way
+words = ["hello", "world", "spam", "eggs"]     # 1. words = list with 4 elements
+counter = 0                        # 2.
+max_index = len(words) - 1         # 3.
+while counter <= max_index:     
+    word = words[counter]          # 4.     
+    print(word + "!")     
+    counter = counter + 1          # 5. add 1 to counter, next time through, counter will be item 1 in the list
+
+    
+# Third Way
+words = ["hello", "world", "spam", "eggs"]
+
+for i in range(0,len(words) ):
+    print (words[i] + "!")
+
+# Shorter Way
+words = ["hello", "world", "spam", "eggs"]
+
+for i in words:
+    print(i + "!")
+
+    
+    
+# Example:
+x=[1,2,3,4,5] 
+y=0 
+while y<len(x): 
+    print(x[y]) 
+    y+=1    # 12345
+
+
+# Example:
+# Manipulating a view ;)
+# I would say that this is useful for the ones having problems with loops.
+
+from string import *
+
+chars = ascii_letters + digits + "_"
+pword = (input() or "Simple").replace(" ","")
+new = ""
+
+print("[Program started]\n\n")
+print("Iterating...\n\n")
+print("-->")
+while 1:
+  
+  if new == pword:
+    break
+  
+  else:
+    for i in pword:
+      for j in chars:
+        if i == j:
+          new += i
+          print(f"-->{new}")
+          break
+      
+        else:
+          print(f"[{j}]")
+          continue 
+    
+print(f"\n\nPassword - {new}")
+print("\n[Program finished]")
