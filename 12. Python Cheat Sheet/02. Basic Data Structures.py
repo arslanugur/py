ef9f2333731349cf98c02b07e8fd36cbe55ac38f
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-27 15:35:22 +0300

    Create 02. Basic Data Structures.py

---
+++ 12. Python Cheat Sheet/02. Basic Data Structures.py
@@ -0,0 +1,17 @@
+# TYPE: Boolean
+# Description: The Boolean data type is either True or False. Boolean operators are ordered by priority: not -> and -> or
+# Code:
+# Evaluates to True:
+1 < 2 and 0 <= 1 and 3 > 2 and 2 >= 2 and 1 == 1 and 1 != 0
+# Evaluates to False:
+bool(None or 0 or 0.0 or '' or [] or {} or set())
+# rule: None, 0, 0.0, empty strings, or empty container types evaluate to False 
+
+# TYPE: Integer, Float
+# Description: An Integer is a positive or negative number without decimal point such as 3.
+#              A float is a positive or negativ4e               
+# Code:
+
+# TYPE: String
+# Description:
+# Code:
