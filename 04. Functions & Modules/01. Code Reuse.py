cdf5961ed0398baf6144aa9fe61f6c9844329c01
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-12-01 10:12:27 +0300

    Create 01. Code Reuse.py

---
+++ 04. Functions & Modules/01. Code Reuse.py
@@ -0,0 +1,19 @@
+# REUSING CODE
+# Code reuse is a very important part of programming in any language. Increasing code size makes it harder to maintain.
+# For a large programming project to be successful, 
+# it is essential to abide by the Don't Repeat Yourself, or DRY (easier to Maintain), principle doing of doing this: by using loops. 
+# In this module: functions and modules.
+# Repetitive code is said to abide by the WET principle, which stands for Write Everything Twice, or We Enjoy Typing.
+
+# Already used functions in previous subjects.
+# Any statement that consists of a word followed by information in parentheses is a function call.
+# Examples:
+print("Hello world!")
+range(2, 20)
+str(12)
+range(10, 20, 3)
+
+# The words in front of the parentheses are function names, 
+# The comma-separated values inside the parentheses are function arguments. 
+
+
