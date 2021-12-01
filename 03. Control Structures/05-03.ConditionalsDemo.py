034598b77403aaab61ae2d156746be5c401ba877
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-12-01 16:23:43 +0300

    Update 05-03.ConditionalsDemo.py

--- 03. Control Structures/05-03.ConditionalsDemo.py
+++ 03. Control Structures/05-03.ConditionalsDemo.py
@@ -1,20 +1,18 @@
-#1  userdan isim, yaş ve eğitim biligilerini isteyip 
-#   ehliyet alabilme durumunu kontrolet
-#   ehliyet alabilme koşulu en az 18 ve eğitim durumu lise ya da üni olmalı
-"""
+# Example: Check driving license situation
+# To request a name, age and degree from the user 
+# Driving License Conditions: at least 18 age, at least high school
+
 name = input('your name: ')
 age = int(input('your age: '))
 degree = input('your degree: ')
 
-if (age >= 18) and (degree == 'high school' or degree == 'university'): 
-#yaş 18den büyük ya da eşit olmalı durumu ve bu bize true değeri vermeli
-    print(f'{name}, you can get driving license') #f stringli
+if (age >= 18) and (degree == 'high school' or degree == 'university'):     # this code value must be true
+    print(f'{name}, you can get driving license') # using f string
 else:
     print('you cant get driving license')
-"""
+    
 
-"""
-#ancak neden alınamıyor yazmıyor bu durumda
+# Example: why cant you get driving license
 name = input('your name: ')
 age = int(input('your age: '))
 degree = input('your degree: ')
@@ -26,24 +24,22 @@ if (age >= 18):
         print('you cant get driving license because of degree')
 else:
     print('you cant get driving license because of age')
-"""
 
 
-"""
-#2  öğrencinin iki yazılı bir sözlü notunu alıp hesaplanan ortalmaya göre
-#   not aralığına karşılık gelen not bilgisini yazdır
-#    0 - 24 --> 0
-#   25 - 44 --> 1
-#   45 - 54 --> 2
-#   55 - 69 --> 3
-#   70 - 84 --> 4
-#   85 - 100 --> 5
 
+# Example: print the grade information corresponding to the grade range 
+#          according to the average of two exams and one interview
+#  0 - 24     --> 0
+# 25 - 44     --> 1
+# 45 - 54     --> 2
+# 55 - 69     --> 3
+# 70 - 84     --> 4
+# 85 - 100    --> 5
 exam1 = float(input('examOne: '))
 exam2 = float(input('examTwo: '))
-speech = float(input('speech: '))
+speech = float(input('interviewGrade: '))
 
-average = (exam1 + exam2 + speech) / 3
+average = (exam1 + exam2 + interviewGrade) / 3
 
 if (average >= 0) and (average <= 24):
     print(f'Your average is {average} and your score: 0')
@@ -57,10 +53,9 @@ elif (average >= 70) and (average <= 84):
     print(f'Your average is {average} and your score: 4')
 elif (average >= 85) and (average <= 100):
     print(f'Your average is {average} and your score: 5')
-else:  #(yukardakiler dışında herşeyse)
-    print('you gave a wrong info')
-#aralığımız 100 kadar bunun dışında girilen her bir bilgi yanlış
-"""
+else:                               # all except the value max. 100 above
+    print('you gave a wrong info')  # Range will be maximum 100
+
 
 
 #3 (kullanıcıdan) trafiğe çıkış zmanı alınan bir aracın (araç kaç gündür trafikte)
