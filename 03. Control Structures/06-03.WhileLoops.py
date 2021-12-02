3c3ff5662ea27dbaaa35c12035a68abacb9ea05c
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-12-02 15:32:48 +0300

    Update 06-03.WhileLoops.py

--- 03. Control Structures/06-03.WhileLoops.py
+++ 03. Control Structures/06-03.WhileLoops.py
@@ -1,68 +1,68 @@
 numbers = [1, 2, 3, 4, 5]
-#bunu for döngüsüyle her elemana ulaşabiliyorduk ama elimizde bi liste yoksa
+# we could reach each element with for loop but if we have no list
+# what if to print the numbers from 1 to 100
 
-#1den 100e kadar sayıları ekranda yazdırırsak peki
-
-x = 0  #sayma sıfırdan başlar bunu 1 ya da 2 den de başlayabilir
-#0dan başlayıp 100e kadar gitsin, her döngü durumundayken
-# ekrana sayı yazdırıp birer artıralım
-# ve 100e geldiği zaman döngü otomatik olarak dursun
-# yani koşul hep true vercek, ne zmanki false düştük orda bitecek (100gibi)
-"""1
+x = 0  # counting starts with 0, to 100
+# to increase one by one when the loop works
+# if it comes to 100, the loop stops automatically
+# so that the condition always gives "true", whenever it comes "false", the loop will be over (like 100)
 while True:
-    print(x)  #while ile sonsuz bi döngü üretir, 
-              #yani sürekli 0 yazıyor, bunu belli biyerde kesmek gerekli
-              # x < 100: deriz bu işlem 100 kadar devam eder 
-"""
+    print(x)    # it produces an infinite loop with while, it means it writes 0 all the time
+                # but we need to stop this at certain place
+    x < 100:    # this process continues until 100 
 
-"""2
-while x < 100:  #true üretmeye devam eder ve x<=100 yaparsak 100 de girer sıraya
-    print(x)    #yine sonsuz döngü olur çünkü x değeri değişmeli
-                #bu durumda x = x + 1 ya da x += 1 dersek değişir
-    x += 1   # 99a kadar sayar. ikili gitsin diyorsan x= x + 2
+
+# Example:
+while x < 100:  # keeps producing true and x <= 100 --> not a stop yet
+    print(x)    # happens an infinite loop again. because "x" value should be changed
+                # changes if we write x = x + 1 or x += 1
+    x += 1      # counts until 99. if you want two by two --> x= x + 2
 print('Count is over!')
-"""
-#bütün ifadeleri yazmak zorunda değiliz
-#koşul kullanırız yazmamakiçin
-"""3
+
+# we don't have to write all expressions, we use conditional to avoid typing 
+
+# Example:
 while x < 100:
-    if x % 2 == 0: #x sadece çift sayılar ise yazdır   
+    if x % 2 == 0: # if x is even numbers, prints   
         print(x)             
-    x += 1         #sadece tekler yazılıcaksa if x % 2 == 1
+    x += 1         # to write odd numbers --> if x % 2 == 1
 print('Count is over!')
-"""
-"""4
-while x < 100:
-    if x % 2 == 1: #x sadece tek sayılar ise yazdır       
-        print(f'number odd: {x}')
-    else: #diğer hepsi de çift
-        print(f'number even: {x}')             
-    x += 1         #hem çiftleri hem de tekleri yazdırırsaki yani hepsini
-print('Count is over!')
-"""
 
-"""5
-#userdan bi değer iste, user bilene kadar sormaya devam et
-name = '' #string değer alsın
-while not name: #yukardaki '' boşluk değeri false olarak değerlendirir
-#while yanındaki ifadenin sürekli true olarak üretilmesi demek, falseun tersini almak demek
-#yani 'not name' aslında true'ya karşılık geliyor. bu da true değerindeki kodları sürekli işlet demek
-#name, boşluk karakterine eşit old sürece while döngüsü işlesin
-#yani userdan name için bi input almak lazım
-    name = input('enter ur name: ')
-    #ne zaman isim doğru girilirse print ile hallo blabla diyelim
+
+# Example:
+while x < 100:
+    if x % 2 == 1:  # prints if x is odd numbers       
+        print(f'number odd: {x}')
+    else:           # Otherwise, even numbers is printed
+        print(f'number even: {x}')             
+    x += 1         # both even and odd numbers, so all of them
+print('Count is over!')
+
+
+# Example:
+# to request a value from user, keep asking until the user know that
+name = ''       # name takes a string value
+                # "name" equals ''
+while not name: # the above '' null evaluates as a false 
+# while yanındaki ifadenin sürekli true olarak üretilmesi demek, false'un tersini almak demek
+# producing the expression as true next to while, means getting the inverse of false 
+# 'not name' --> true. means working codes which is true
+
+# input for "name" from the user
+    name = input('enter your name: ')
+    # ne zaman isim doğru girilirse print ile hallo blabla diyelim
 print(f'Hallo {name}')
 
-#inputa bi değer girmediğiniz sürece 'enter ur name' ekrana çıkar
-#çünkü name bilgisinin boşluk karakterine eşit olması false'a eşit
+# inputa bi değer girmediğiniz sürece 'enter ur name' ekrana çıkar
+# çünkü name bilgisinin boşluk karakterine eşit olması false'a eşit
 
-#bi değer girmek yerie boşluk(spacebar) karakterine basarsa boş gözükür
-#'' bu boş bi str ifade
-#' ' içinde boş bir karakter var
-#bunu engellemek için ise while not name.strip(): yazarız.
-#böylece spacebar kabul etmez
-"""
+# bi değer girmek yerie boşluk(spacebar) karakterine basarsa boş gözükür
+# '' bu boş bi str ifade
+# ' ' içinde boş bir karakter var
+# bunu engellemek için ise while not name.strip(): yazarız.
+# böylece spacebar kabul etmez
 
+# Example:
 while False:
     print('Looping..') # 0 değerde sonuç çıkmaz
 
