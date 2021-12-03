645a35b195d324356424b05aa68e01d68684b842
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-12-03 13:42:57 +0300

    Update and rename 04-03.CompareOperators.py to 09. Comparison Operators.py

---
+++ 01. Basic Concepts/09. Comparison Operators.py
@@ -0,0 +1,32 @@
+# Comparison Operators
+# username, password  --> these will save into database
+# if the user comes again, request them as input
+# 'arslanugur' , '12345'
+# uses comparison operators for them
+
+a, b, c, d = 5, 5, 10, 4
+# to define values
+username = 'arslanugur'
+password = '12345'
+
+result = a == b       # to ask a equals b, we keep this in 'result'
+result = a != b       # the reverse question --> a not equals b operator
+result = (a == c)     # if we increase conditions, we can use parenthesis
+result = (a != b)     # result will be true, because not equal
+result = (a > c)
+result = (a >= c)
+result = (True == 1)  # equals true --> 1 as numerical
+result = (False == 0) # true
+result = False+ True+33 # result value will be 34
+                        # a = b ---> b == a
+                        # result will be true or false
+print(result)
+
+
+result = 'arsnugr' == username      # about logical operators
+                                    # to ask both username and password 
+                                    # requests two questions and two answers
+print(result)
+
+
+
