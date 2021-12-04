69431683471ea2c9285c1ac5fa34a29705673882
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-12-04 14:41:38 +0300

    Update 10. Demo: Comparasion Operators.py

--- 01. Basic Concepts/10. Demo: Comparasion Operators.py
+++ 01. Basic Concepts/10. Demo: Comparasion Operators.py
@@ -3,59 +3,47 @@ numbera = int(input('number a: '))
 numberb = int(input('number b: '))
 
 result = numbera > numberb # it will be true-false - if the value true, the value  olursa a değeri büyük
-print(f'number a: {numbera} number b: {numberb} den büyük: {result}') 
-#süslü parantez sayı değerini vercek
-#true ya da false değeri kullanıcıya gösterilmez ama
-"""
+print(f'number a: {numbera} is bigger than number b: {numberb}: {result}') 
 
-"""
-#2 kullanıcıdan iki vize (%60) ve bir final (%40) notu al ortalama hesapla
 
-vize1 = float(input('1st midterm: '))
-vize2 = float(input('2nd midterm: '))
+
+# Example: from the user, calculate the average of two midterms (%60) and one final (%40) grades
+midterm1 = float(input('1st midterm: '))  # may input decimal number, thats why using float 
+midterm2 = float(input('2nd midterm: '))
 final = float(input('final: '))
 
-# aldığımız bilgiyi floata çevir çünkü ondalıklı bi sayı da girilebilir
 
-#3 ortalama 50 ve üstündeyse geçti değilse kaldı yazdır
-#üsteki bilgilerin ortalaması
-average = (((vize1 + vize2) / 2) * 0.6) + (final * 0.4)
-print(f'Not ortalamanız: {average} ve dersten geçme durumunuz: {average>=50} ')
-#50den büyük ya da eşitse geçti/true, altında ise kaldı/false
-#bu konuları if bloklarında daha sağlıklı görcen
-"""
+# Example: if 'average' equals 50 and above (true), it will pass. otherwise fail (false)
+average = (((midterm1 + midterm2) / 2) * 0.6) + (final * 0.4)
+print(f'Grade Average: {average} and Passing Status: {average>=50} ')
+# if blocks could be better for this problem
 
-"""
-#4 girilen bir sayını tek mi çift mi olduğunu yazdır
 
+# Example: to print whether a inputted number is odd or even
 number = int(input('number: '))
+oddeven = (number % 2 == 0) # modulus equals 0, then even --> by dividing results 0
+print(f'situation of the entered number: {oddeven}')
 
-oddeven = (number % 2 == 0)
-#girilen sayının mod2 sini aldığımızda sonuç sıfıra eşitse sayı çift yani bölüne bölüne 0 kalır
-print(f'girilen sayının çift olma durumu: {oddeven}')
-"""
 
-"""
-#5 girilen bir sayının pozitif negatif durumunu yazdır
 
+# Example: to print whether a inputted number is positive or negative
 number = int(input('number: '))
-positive = (number > 0) #true gelirse demek ki pozitif, eksi değer girilirse false
-print(f'girilen sayının pozitif olma durumu: {positive}')
-"""
+positive = (number > 0) # if it comes true --> positive, false --> negative
+print(f'situation of the entered number: {positive}')
 
-#6 parola ve mail bilgisi iste, doğruluğunu kontrol et
-# arslan@gmail.com password: arslan --> bunlar kullanıcının kaydı ve database de durur
-email = 'arslan@gmail.com' 
-password = 'arslan'
 
-girilenMail = input('email: ') #zaten str olsuğu için dönüşüm yapmak gereksiz
-girilenPass = input('password: ')
+# Example: to request password and mail info, check authenticity
+# mail@gmail.com password: mail --> user registry from database
+email = 'mail@gmail.com' 
+password = 'mail'
 
-isMail = email  == girilenMail
-#isMail = (email  == girilenMail.lower()) #yanlışlıkla büyük yazılabilir diye otomatik küçültür
-#isMail = (email  == girilenMail.lower().strip()) #yanlışlıkla hem büyük hem de sonda boşluk bırakırsa düzeltir
-#str başına yanına sonuna girdiğimiz boşluklar strip metoduyla silinir
-isPass = password == girilenPass    #bu sorulara true ya da false gelcek
-print(f'Email bilgisi doğrumu: {isMail} ve parola doğru mu: {isPass} ')
+enteredMail = input('email: ') # input will be already string, conversion is not necessary
+enteredPass = input('password: ')
 
+isMail = email  == enteredMail
+# isMail = (email  == enteredMail.lower())         # in case it is written with upper letter wrongly, we use the 'lower' method
+# isMail = (email  == enteredMail.lower().strip()) # in case it is written with upper letter wrongly and use unnecessary space wrongly, we use the functions 'lower' and 'strip'
+# deletes the spaces we inputted value the begin, the end of 'str' with strip method
+isPass = password == enteredPass    # comes true / false to the questions
+print(f'email info verification: {isMail} and password verification: {isPass} ')
 
