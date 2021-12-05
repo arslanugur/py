727efae80eb0c118c29b8fb32eea4d615d282194
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-12-05 04:30:10 +0300

    Create 03. Function Arguments.py

---
+++ 04. Functions & Modules/03. Function Arguments.py
@@ -0,0 +1,12 @@
+# Arguments
+  # All the function definitions we've looked at so far have been functions of zero arguments, which are called with empty parentheses.
+  # However, most functions take arguments.
+  # The example below defines a function that takes one argument:
+#
+def print_with_exclamation(word): # the argument is defined inside the parentheses.
+   print(word + "!")
+    
+print_with_exclamation("exclamation")
+print_with_exclamation("explaination")
+print_with_exclamation("examination")
+
