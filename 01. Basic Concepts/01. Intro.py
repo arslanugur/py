b418e6bedc22123d1e59af1ff8a91ad53a68f6e3
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-12-08 16:24:59 +0300

    Update 01. Intro.py

--- 01. Basic Concepts/01. Intro.py
+++ 01. Basic Concepts/01. Intro.py
@@ -57,5 +57,34 @@ Hobbies =" Coding"
 Print(myName+ " loves "+ Hobbies) # Output: He loves Coding
     
 # Comments and Docstrings   
-  
-  
+# In Python, a comment is created by inserting an octothorpe (otherwise known as a number sign or hash symbol: #). All text after it on that line is ignored.
+# For example:
+x = 365
+y = 7
+# this is a comment
+
+print(x % y) # find the remainder
+# print (x // y)
+# another comment
+
+# Python doesn't have general purpose multiline comments, as do programming languages such as C.
+
+
+
+# Docstrings
+# Docstrings (documentation strings) serve a similar purpose to comments, as they are designed to explain code. 
+# However, they are more specific and have a different syntax. 
+# They are created by putting a multiline string containing an explanation of the function below the function's first line
+# Example:
+def shout(word):
+    """
+    Print a word with an
+    exclamation mark following it.
+    """
+    print(word + "!")
+
+shout("spam")
+
+# Unlike conventional comments, docstrings are retained throughout the runtime of the program. 
+# This allows the programmer to inspect these comments at run time.
+
