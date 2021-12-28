9333dd9e5ac818b8708f8ca04039f3ddd1426cef
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-12-28 20:48:06 +0300

    Create 00. String Formatting.py

---
+++ 11. Advanced Python/00. String Formatting.py
@@ -0,0 +1,9 @@
+
+%d – integer
+%f – float
+%s – string
+%x – hexadecimal
+%o – octal
+# String Format -- https://www.it-swarm-tr.com/tr/python/python-string-formatlamada-s-ve-d-arasindaki-fark-nedir/970513249/
+
+https://www.geeksforgeeks.org/string-formatting-in-python-using/
