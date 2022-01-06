9b67476273d267f19b2fdaba2b1c31a064e083d9
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-01-06 22:30:07 +0300

    Create 09. Print Output Without a Newline.py

---
+++ 13. Examples/09. Print Output Without a Newline.py
@@ -0,0 +1,25 @@
+# To print output without a newline.
+
+# Using end keyword
+
+# print each statement on a new line
+print("Python")
+print("is easy to learn.")
+
+# new line
+print()
+
+# print both the statements on a single line
+print("Python", end=" ")
+print("is easy to learn.")
+
+# Output:
+# Python
+# is easy to learn.
+
+# Python is easy to learn.
+
+# Using the end keyword, you can append a string at the end of the print text. 
+# In the above example, we have passed a space with end, which adds a space at the end of the line and concatenates the content of the next print statement.
+
+
