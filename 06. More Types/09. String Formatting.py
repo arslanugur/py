c4b03249c1c99594d1faab52d544cec35ff8a414
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-01-16 22:28:32 +0300

    Create 09. String Formatting.py

---
+++ 06. More Types/09. String Formatting.py
@@ -0,0 +1,16 @@
+# Section 1
+# So far, to combine strings and non-strings, you've converted the non-strings to strings and added them.
+# String formatting provides a more powerful way to embed non-strings within strings. 
+# String formatting uses a string's format method to substitute a number of arguments in the string.
+# Example:
+nums = [4, 5, 6]
+msg = "Numbers: {0} {1} {2}". format(nums[0], nums[1], nums[2])
+print(msg)
+# Numbers: 4 5 6
+
+# Each argument of the format function is placed in the string at the corresponding position, which is determined using the curly braces { }.
+
+# Comme.....
+
+
+
