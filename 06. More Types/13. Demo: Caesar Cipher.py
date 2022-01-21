479dde8e001ccc1c0fb15847e1b21a8525de50af
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-01-21 17:35:58 +0300

    Update 13. Demo: Caesar Cipher.py

--- 06. More Types/13. Demo: Caesar Cipher.py
+++ 06. More Types/13. Demo: Caesar Cipher.py
@@ -1,3 +1,4 @@
+# FIRST WAY
 def rot(character, key):
     size_of_alphabet = ord('Z') - ord('A') + 1
     key %= size_of_alphabet
@@ -134,7 +135,44 @@ def numConv(num):
 print(tdec(text))
 
 
+# THIRD WAY
+# I edited line 33
+def count_char(text, char):
+    count = 0
+    for c in text:
+        if c == char:
+            count += 1
+    return count
 
+file = open("newfile.txt", "w")
+file.write("""Ornhgvshy vf orggre guna htyl.
+Rkcyvpvg vf orggre guna vzcyvpvg.
+Fvzcyr vf orggre guna pbzcyvpngrq.
+Syng vf orggre guna arfgrq.
+Fcenfr fv orggre guna qrafr.
+Ernqnovyvgl pbhagf.
+Fcrpvny pnfrf nera'g fcrpvny rabthu gb oernx gur ehyrf.
+Nygubhtu cenpgvpnyvgl orgnf chevgl.
+Reebef fubhyq arire cnff fvyragyl.
+Hayrff rkcyvpvgyl fvyraprq.
+Va gur snpr bs nzovthvgl, ershfr gur grzcgngvba bg thrff.
+Gurer fubhyq or bar-- naq cersrenoylbayl bar --boivbhf jnl gb qb vg.
+Nygubhtu gung jnl znl abg or boivbhf ng svefg hayrff lbh'er Qhgpu.
+Abj vf orggre guna arrire.
+Nygubhtu arire vf bsgra orggre guna *evtug* abj.
+Vs gur vzcyrzragngvba vf uneq gb rkcynva, vg'f n onq vqrn.
+Vs gur vzcyrzragngvba vf rnfl gb rkcynva, vg znl or n tbbq vqrn.
+Anzrfcnprf ner bar ubaxvat terng vqrn -- yrg'f qb zber bs gubfr!""")
+file.close()
+filename = "newfile.txt"
+with open(filename) as f:
+    text = f.read()
+
+for char in sorted(set(text)):
+    perc = 100 * count_char(text, char) / len(text)
+    print("{0} - {1}%".format(char, round(perc, 2)))
+#    
+    
 
 
 
