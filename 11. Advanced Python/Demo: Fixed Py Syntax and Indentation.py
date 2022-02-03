e4fc0056c7a8ba1a704ca58286cf5f28421c51de
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-02-03 05:00:15 +0300

    Create Demo: Fixed Py Syntax and Indentation.py

---
+++ 11. Advanced Python/Demo: Fixed Py Syntax and Indentation.py
@@ -0,0 +1,9 @@
+# Fixed Py Syntax and Indentation
+
+# Fix the Style and Syntax so it prints the values accordingly
+bool_list = [[1,1], [1,0], [0,1], [0,0]]
+for x,y in bool_list:
+  print(x, y, end='\n')
+#
+
+
