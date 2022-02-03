ec9d39800af3786065f4b1f6fe7534c853e62290
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-02-03 18:27:38 +0300

    Update 14. itertools.py

--- 07. Functional Programming/14. itertools.py
+++ 07. Functional Programming/14. itertools.py
@@ -136,17 +136,106 @@ for i in "python":
 # Format: import {module-name} or from {module-name} import {class/function}
 
 
+# SECTION 2
+# There are many functions in itertools that operate on iterables, in a similar way to map and filter.
+# Some examples:
+# takewhile - takes items from an iterable while a predicate function remains true;
+# chain - combines several iterables into one long one;
+# accumulate - returns a running total of values in an iterable. 
+# Example:
+from itertools import accumulate, takewhile
 
-https://www.sololearn.com/learning/1073/2466/5117/1
+nums = list(accumulate(range(8)))
+print(nums)                                     # [0, 1, 3, 6, 10, 15, 21, 28]
+print(list(takewhile(lambda x: x<= 6, nums)))   # [0, 1, 3, 6]
+# 0+1, 1+2, 3+3, 6+4, 10+5, 15+6, 21+7, 28+8    # Total + NextSequenceInRange
+
+# accumulate goes like this:
+# if 0 is there, add numbers up to 0(0+0=0) 
+# if 1 is there, add numbers up to 1(0+1=1) 
+# if 2 is there, add numbers up to 2(0+1+2=3) 
+# if 3 is there, add numbers up to 3(0+1+2+3=6) 
+# if 4 is there, add numbers up to 4(0+1+2+3+4=10) and so on
+
+# Example:
+print(list(chain([1,3,2], [3,5,9]))) 
+print(list(chain(["man","nan"], ["pan"]))) # output: [1, 3, 2, 3, 5, 9] ['man', 'nan', 'pan'] just to illustrate an example of the CHAIN function
+
+# Difference between filter and takewhile is that filter takes all the elements which satisfy the predicate function 
+# while takewhile will stop taking elements the moment an element doesn't satisfy the predicate function
+
+# A way to help you understand lessons and improve your problem solving skills is to read the program and its results, 
+# then type out the program yourself and go over it line by line. 
+# Why does it print what it prints? in this case: nums = list(accumulate(range(8))) 
+# nums holds the value list(accumulate(range(8))) 
+# list is telling it to print in list form (i.e. in brackets []) 
+# accumulate is saying that it is going to return the individual values add to it's predecessor one at a time 
+# range is just limiting it to remain in the range of 8 (i.e. 0-7) 
+# print(nums) will produce: >>> [0, 1, 3, 6, 10, 15, 21, 28] 
+# Breakdown: 
+# range = 0, 1, 2, 3, 4, 5, 6, 7 
+# so 0=0, 1+0=1, 2+1=3, 3+3=6, 4+6=10, 5+10=15, 6+15=21, 7+21=28 
+# it moves through the range one at a time ACCUMULATING as it processes. 
+# print(list(takewhile(lambda x: x<= 6, nums))) 
+# x is the placeholder for nums """ 
+# it will take the result of nums printed and print the sequence as long as the output is equal to or less than 6. the lambda is our parameter to set
+
+# Example: 
+from itertools import takewhile,chain 
+mylist = [0,1,3,6,10,15,2,1] 
+print(mylist) # [0, 1, 3, 6, 10, 15, 2, 1]
+a = lambda x:x<=6 
+tw = list(takewhile(a,mylist)) 
+fl = list(filter(a,mylist)) 
+ch = list(chain(tw,fl)) 
+print(tw)   # [0, 1, 3, 6]
+print(fl)   # [0, 1, 3, 6, 2, 1]
+print(ch)   # [0, 1, 3, 6, 0, 1, 3, 6, 2, 1] 
 
 
+# the difference between takewhile and filter 
+# filter goes through all the elements of the iterabele takewhile checks the condition as long as it is True. when it hit False, it exits the iterable.
+
+# print(list(takewhile(lambda x: x<=6, nums))) 
+# This takes the values of nums and puts them into the variable x one at a time. 
+# For the values that result in the expression being true it prints the value in a list.
+
+# takewhile-get the values of the sequence, until the value of the predicate function for its elements is true; 
+# dropwhile-receiving sequence values starting with an element for which the value of the predicate function will cease to be true. 
+# From itertools import takewhile, dropwhile Numbers = [1, 5, 8, 5, 1] Predicate = lambda x: x <6 For value in takewhile (predicate, numbers): 
+# Print (value) Print () For value in dropwhile (predicate, numbers): Print (value) Output >>> 1 >>> 5 >>> 8 >>> 5 >>> 1
 
 
-BEGINNER LEVEL PROGRAMMING PROJECTS 
-https://www.sololearn.com/post/4869/?ref=app 
-  https://www.sololearn.com/post/4870/?ref=app 
-    https://www.sololearn.com/post/4873/?ref=app 
-      https://www.sololearn.com/post/4874/?ref=app
+# I found some facts about takewhile function. 
+# I experimented with four operators(<, <=, >, >=) and those working normally in 'takewhile' were < and <=. 
+# I think it's because the iteration process starts with the first element. 
+# So if the first element doesn't satisfy the predicate function, it won't start any iteration. Hence all you can see is an empty list. 
+# However, this is not the case when the condition changes. 
+# If you change <= to > and 6 to -1 in this example, you will see the whole list. 
+# In conclusion, all four operators are valid in takewhile function. 
+# But what makes the output valid is whether the predicate function is satisfied or not.
+
+# Idk why an example of chain is not shown on the lesson, but here it goes: 
+from itertools import accumulate, takewhile, chain 
+nums = list(accumulate(range(8))) 
+print(nums)                                                                 # [0, 1, 3, 6, 10, 15, 21, 28]
+print(list(takewhile(lambda x: x<= 6, nums)))                               # [0, 1, 3, 6] 
+print(list(chain(accumulate(range(8)),takewhile(lambda x: x<= 6, nums))))   # [0, 1, 3, 6, 10, 15, 21, 28, 0, 1, 3, 6] 
+
+
+# Example:
+from itertools import accumulate 
+def sigma(n): '''Returns sum of integers from 0 to n''' 
+    if n == 0: 
+        return 0 
+    return n + sigma(n-1) lst1 = [sigma(i) for i in range(8)] 
+lst2 = list(accumulate(range(8))) print(lst1 == lst2)
+    
+range(8) = 0,1,2,3,4,5,6,7 accumulate = 0,0+1,0+1+2,0+1+2+3,0+1+2+3+4,0+1+2+3+4+5,0+1+2+3+4+5+6,0+1+2+3+4+5+6+7
+
+
+https://www.sololearn.com/learning/1073/2466/5117/2
+
 
         
         
