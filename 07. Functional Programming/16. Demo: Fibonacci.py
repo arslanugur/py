19e93fe2411f80d614e8d454c5e88cde396c90af
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-02-09 12:24:48 +0300

    Create 16. Demo: Fibonacci.py

---
+++ 07. Functional Programming/16. Demo: Fibonacci.py
@@ -0,0 +1,31 @@
+# Fibonacci
+# The Fibonacci sequence is one of the most famous formulas in mathematics.
+# Each number in the sequence is the sum of the two numbers that precede it.
+# For example, here is the Fibonacci sequence for 10 numbers, starting from 0: 0,1,1,2,3,5,8,13,21,34.
+
+# Write a program to take N (variable num in code template) positive numbers as input, 
+# and recursively calculate and output the first N numbers of the Fibonacci sequence (starting from 0). 
+
+# Sample Input: 6
+# Sample Output: 0 1 1 2 3 5 
+
+# If you are making the Fibonacci sequence for n numbers, you should use n<=1 condition as the base case.
+
+# Code:
+num = int(input())
+fib=list(range(num))
+def fibonacci(n):
+	if n<=2: 
+	
+	 return fib[n-1]
+	   
+	else:
+	  fib[n-1]=fibonacci(n-1)+fibonacci(n-2)
+	  return fib[n-1]
+
+(fibonacci (num))
+for i in fib:
+	  print(i)
+#
+
+
