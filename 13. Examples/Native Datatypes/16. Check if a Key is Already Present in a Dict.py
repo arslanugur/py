56e45b94221fba9eece40110922b42f55efc636c
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-02-26 16:51:43 +0200

    Create 16. Check if a Key is Already Present in a Dict.py

---
+++ 13. Examples/Native Datatypes/16. Check if a Key is Already Present in a Dict.py
@@ -0,0 +1,15 @@
+# Native Datatypes Examples
+# Example: to Check if a Key is Already Present in a Dictionary
+
+# Example:
+# Using in keyword
+my_dict = {1: 'a', 2: 'b', 3: 'c'}
+
+if 2 in my_dict:
+    print("present")    # Output: present
+
+# Using if statement and in keyword, you can check if a key is present in a dictionary.
+# In the above example, 2 is present in the dictionary as a key; therefore, the output is present.
+# You can use not in if you want to check if a key is not present in the dictionary.
+
+
