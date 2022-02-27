b01773b30acb5fb3965702de2d311115459c7292
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-02-27 09:23:51 +0200

    Create 21. Get the Last Element of the List.py

---
+++ 13. Examples/Native Datatypes/21. Get the Last Element of the List.py
@@ -0,0 +1,20 @@
+# Native Datatypes Examples
+# Example: to Get the Last Element of the List
+
+# Using negative indexing
+my_list = ['a', 'b', 'c', 'd', 'e']
+
+# print the last element
+print(my_list[-1])        # Output: e
+
+# When you use negative indexing, the counting starts from 1 not 0 as shown in the figure below.
+
+# List indexing in Python
+#                    length = 5
+#                 'p''r''o''b''e'
+# index            0  1  2  3  4
+# negative index  -5 -4 -3 -2 -1
+
+# If you want the first 1st element, you can use my_list[-5].
+
+
