b0e9614289f2db43f62c5ba1b42787aca4c7b082
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-02-27 09:56:11 +0200

    Create 25. Count the Occurrence of an Item in a List.py

---
+++ 13. Examples/Native Datatypes/25. Count the Occurrence of an Item in a List.py
@@ -0,0 +1,11 @@
+# Native Datatypes Examples
+# Example: to Count the Occurrence of an Item in a List
+
+# Using count() method
+freq = ['a', 1, 'a', 4, 3, 2, 'a'].count('a')
+print(freq)       # 3
+
+# Using count() method, pass the item to be counted. As shown above, 'a' is passed, which gives the total number of occurrences of character 'a'.
+# You can learn more about count() at Python count().
+
+
