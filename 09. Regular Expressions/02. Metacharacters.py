a1f328f41cfdb4ffffdb0a0de64f035e59438c31
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-05 11:48:25 +0300

    Create 02. Metacharacters.py

---
+++ 09. Regular Expressions/02. Metacharacters.py
@@ -0,0 +1,12 @@
+# SECTION 1
+# Metacharacters are what make regular expressions more powerful than normal string methods.
+# They allow you to create regular expressions to represent concepts like "one or more repetitions of a vowel".
+
+# The existence of metacharacters poses a problem if you want to create a regular expression (or regex) that matches a literal metacharacter, such as "$". 
+# You can do this by escaping the metacharacters by putting a backslash in front of them.
+# However, this can cause problems, since backslashes also have an escaping function in normal Python strings. 
+# This can mean putting three or four backslashes in a row to do all the escaping.
+# To avoid this, you can use a raw string, which is a normal string with an "r" in front of it. We saw usage of raw strings in the previous lesson.
+
+
+https://www.sololearn.com/learning/1073/2476/5159/1
