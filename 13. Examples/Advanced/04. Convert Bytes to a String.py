590a157681e03d6aca45a287b823412509f266c4
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-05 14:53:34 +0300

    Create 04. Convert Bytes to a String.py

---
+++ 13. Examples/Advanced/04. Convert Bytes to a String.py
@@ -0,0 +1,14 @@
+# Advanced Examples
+# Example: to Convert Bytes to a String
+# Topics: Python Input, Output and Import
+
+# Using decode()
+print(b'Easy \xE2\x9C\x85'.decode("utf-8"))
+
+# Output: Easy ✅
+
+# Using decode(), you can convert bytes into string. Here, we have used utf-8 for decoding. \xE2\x9C\x85 is the utf-8 code for ✅.
+
+# You can learn about encoding on Python String encode().
+
+
