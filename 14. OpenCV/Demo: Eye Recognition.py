523ca5d3c0e5f0785c9230ca25aa5e6c6f4bd583
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-09 16:12:04 +0300

    Update Demo: Eye Recognition.py

--- 14. OpenCV/Demo: Eye Recognition.py
+++ 14. OpenCV/Demo: Eye Recognition.py
@@ -1,1 +1,14 @@
 import cv2
+
+faceCascade_ = cv2.CascadeClassifier('Cascades/haarcascade_frontalface_deafault.xml')
+eyeCascade_ = cv2.CascadeClassifier('Cascades/haarcascade_eye.xml')
+
+camera_ = cv2.VideoCapture(0)
+camera_.set(3, 1280)          # Genişlik
+camera_.set(4, 720)           # YÜkseklik
+fileName_ = None              # 'göz_saptama.mp4'
+recorder_ = None
+
+while True:
+  _, square_ = camera_.read()
+  
