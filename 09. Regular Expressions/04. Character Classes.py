eed334ff870729d9c30adcbcc1b3cb8559bcbb77
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-11 15:39:04 +0300

    Update 04. Character Classes.py

--- 09. Regular Expressions/04. Character Classes.py
+++ 09. Regular Expressions/04. Character Classes.py
@@ -179,23 +179,92 @@ if re.search(pattern, "1ab"):
 
 # The pattern in the example above matches strings that contain two alphabetic uppercase letters followed by a digit.
 
+# Example:
+import re 
+pattern = r"[A-Z][A-Z][0-9]" 
+search1 = re.search(pattern, "LS8") 
+search2 = re.search(pattern, "E3") 
+search3 = re.search(pattern, "AC130") 
+if search1: 
+    print("Match 1", search1.group()) 
+if search2: 
+    print("Match 2", search2.group()) 
+if search3: 
+    print("Match 3", search3.group())
+# you can validate an email address by using this code. and ofc you can modify it according to your need. 
+import re 
+email = r"[a-z A-Z 0-9]+[@]+[a-z]+[.]+[a-z]" 
+if re.search(email, "Bilal123@gmail.com"): 
+    print("Valid Email Address") 
+else: 
+    print("Invalid email address! try again")
+#
 
-https://www.sololearn.com/learning/1073/2478/5163/1 comments
+# Example:
+import re 
+pattern = r"[A-Z][a-z][0-9]" 
+if re.search(pattern, "LS8"): 
+    print("Match 1") 
+if re.search(pattern, "E3"): 
+    print("Match 2") 
+if re.search(pattern, "Dg1"): 
+    print("Match 3")            #
+#
+
+# Example:
+import re 
+password = input("Enter your password: ") 
+isStrong = re.search(r"[A-Z]", password); 
+isStrong = isStrong and re.search(r"[a-z]", password) 
+isStrong = isStrong and re.search(r"[0-9]", password) 
+if isStrong: 
+    print("Your password is strong.") 
+else: 
+    print("Your password is too weak!") 
+# Enter your password: aa5                      Your password is too weak! 
+# Enter your password: SECRETpassword1234       Your password is strong.
+
+# So the match can be anywhere in str and not just the first 3 chars 
+import re 
+pattern = r"[A-Z][a-z][0-9]" 
+if re.search(pattern, "LS8"): 
+    print("Match 1") 
+if re.search(pattern, "Re3"): 
+    print("Match 2") 
+if re.search(pattern, "1abRe3"): 
+    print("Match 3") 
+# nxt pge example, removed ending square bracket and still came up with output Match 1 Match 2 Match 3 why didn't it error out with typo? 
+# Note missing end bracket after 5 
+import re 
+pattern = r"[1-5[0-9]" 
+if re.search(pattern, "59"): 
+    print("Match 1") 
+if re.search(pattern, "5900000"): 
+    print("Match 2") 
+if re.search(pattern, "123459"): 
+    print("Match 3")
+#
+
+# What would [1-5][0-9] match? -- Any two-digit number from 10 to 59
 
 
+The answer to this is a little tricky but easy when you get it The first letter is from 1-5 And the second one from 0-9 (the 0 can represent numbers like 10/20/30/40/50) Not above 50 because the highest first digit possible is 5 So hope I helped abit 
 
+[1-5] Min value:1 Max value:5 [0-9] Min value:0 Max value:9 Valid number range: 10 to 59
 
+                    Why is it printed??? import re pattern = r"[10-59]" if re.search(pattern, "62"): print("Match") # OUTPUT: Match
+  
+  [1-5] means 1 2 3 4 5 [0-9] means 0 1 2 3 4 5 6 7 8 9 So answer is any two digits number # from 10 to 59
+  
+[1-5][0-9] Number first digit 1 to 5 and second digit is 0 to 9 means 10,11,......,19 20,21,...,28,29 ...... 50,51,...,58,59 so it means 10-59 numbers 
+  
+import re pattern = r"[1-5][0-9]" if re.search(pattern, "10"): print("Match 1") if re.search(pattern, "59"): print("Match 2") if re.search(pattern, "1100"): print("Match 3") The output of this code is Match1 Match2 Match3 So I would like to conclude that the range is not fixed our first 2 digits matches with criteria, the range can be anything for e g. 1100, 5917, 4809 etc. 
+            
+            
+# SECTION 3
 
-
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+https://www.sololearn.com/learning/1073/2478/5164/1
+    
+    
+    
   
