9e0a6373941d79e296c02d66fcfdf292e67acef2
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-14 18:20:28 +0300

    Update 09. Groups.py

--- 09. Regular Expressions/09. Groups.py
+++ 09. Regular Expressions/09. Groups.py
@@ -1,10 +1,10 @@
+# lesson should be before the metacharacters
 # SECTION 1
 # A group can be created by surrounding part of a regular expression with parentheses.
 # This means that a group can be given as an argument to metacharacters such as * and ?.
 # Example:
 import re
-
-pattern = r"egg(spam)*"
+pattern = r"egg(spam)*"                 # The string has to start "egg" and be followed by "spam" zero or more times
 
 if re.match(pattern, "egg"):
     print("Match 1")  #
@@ -18,5 +18,17 @@ if re.match(pattern, "spam"):
 
 # (spam) represents a group in the example pattern shown above. 
 
-https://www.sololearn.com/learning/1073/2480/5169/1 comments
-  
+import re 
+pattern = r"egg(spam)*" 
+if re.search(pattern, "egg"): 
+    print("Match 1") #
+if re.findall(pattern, "eggspamspamspamegg"): 
+    print("Match 2") #
+if re.finditer(pattern, "spam"): 
+    print("Match 3") #
+#
+
+# What would '([^aeiou][aeiou][^aeiou])+' match? -- One or more repetitions of a non-vowel, a vowel and a non-vowel
+https://www.sololearn.com/learning/1073/2480/5169/2   comments
+
+    
