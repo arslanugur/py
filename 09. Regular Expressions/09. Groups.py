1e5be175023a82b03db59bab5ae0fd071e6ffc40
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-15 18:14:41 +0300

    Update 09. Groups.py

--- 09. Regular Expressions/09. Groups.py
+++ 09. Regular Expressions/09. Groups.py
@@ -29,6 +29,60 @@ if re.finditer(pattern, "spam"):
 #
 
 # What would '([^aeiou][aeiou][^aeiou])+' match? -- One or more repetitions of a non-vowel, a vowel and a non-vowel
-https://www.sololearn.com/learning/1073/2480/5169/2   comments
 
+# It would have been more accurate 
+# to say it would match one or more repetitions of a [non vowel or an "uppercase" vowel], [a "lower case" vowel], 
+# and again [a non-vowel or an "uppercase" vowel].
+
+# "Nothing" will match. "Not", "hin" match the pattern.
+
+# the ^ means anything else apart from what is stated in that particular group, 
+# on the other hand the meta character"+" means one or more repetitions,
+# therefore the answer is one more repetition of a vowel and non- vowel so easy.
+
+
+# SECTION 2
+# The content of groups in a match can be accessed using the group function.
+# A call of group(0) or group() returns the whole match.
+# A call of group(n), where n is greater than 0, returns the nth group from the left.
+# The method groups() returns all groups up from 1.
+# Example:
+import re
+pattern = r"a(bc)(de)(f(g)h)i"
+match = re.match(pattern, "abcdefghijklmnop")
+if match:
+    print(match.group())    # abcdefghi
+    print(match.group(0))   # abcdefghi
+    print(match.group(1))   # bc
+    print(match.group(2))   # de
+    print(match.groups())   # ('bc', 'de', 'fgh', 'g')
+#
+
+# As you can see from the example above, groups can be nested
+
+1st Group = bc 
+2nd Group = fgh 
+3rd Group = g 
+4th Group = de 
+# In the following code example, note how the groups appear to be given by the above. 
+import re 
+pattern = r"a(bc)(f(g)h)(de)i" 
+match = re.match(pattern, "abcfghdeijklmnop") 
+if match: 
+    print(match.group()) # the entire MATCH 
+    print(match.group(0)) 
+    print(match.group(1)) # the 1st GROUP 
+    print(match.group(2)) #the 2nd GROUP 
+    print(match.group(3)) #the 3rd GROUP 
+    print(match.group(4)) #the 4th GROUP 
+    print(match.groups()) #ALL GROUPs!!! OUTPUT: >> abcfghdei abcfghdei bc fgh g de ('bc', 'fgh', 'g', 'de')
+#    
+
+
+https://www.sololearn.com/learning/1073/2480/5170/1 coomentsa
     
+
+
+
+
+
