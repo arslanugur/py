ee811477c208e98b6f7bafb819aa23a9425239ab
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-22 01:13:57 +0300

    Create Kodland Quiz..py

---
+++ Kodland Quiz..py
@@ -0,0 +1,58 @@
+
+
+"""
+for i in range(1, 5):
+    print(i, "multiplied by 8=",i*8)
+# how many rows of the table will be displayed?
+"""
+
+# what the elif function means? -- checking another conditon
+
+"""
+a = 5
+b = 6
+if a == 127 or b != 18:
+"""
+
+
+# which function can be used to convert a number into string format? --- str()
+# What does the function // mean? --- //	Division (floor): divides the first operand by the second ---- integer division
+
+# what does a break instruction do if it is triggered in a loop? -- interrupts the cycle
+
+
+# how many different options will the following cycle check?
+for a in range(0,2):
+    for b in range(0,2):
+        for c in range(0,2):
+            for d in range(0,2):
+                for g in range(0,2):
+                    for f in range(0,2):
+
+
+# A message was received from a child:
+# "Hello, I can't understand how the programme works. Please Explain"
+# Write a reply the the child
+games = []
+while True:
+  a = input()
+  if a == '':
+    break
+  else:
+    games.append(a)
+print(games)
+  
+
+
+
+
+
+
+
+#1 print(type(42.0))
+#2 print(float('fifty-two and a half'))    # Error
+#3 what function is used to stop the turtleto leave a trail -- penup -- up()
+# which of the names for the variables is not acceptable-   2Teacher
+
+# x = 2.5e6
+# print(x)
