9bfe58d139984d7918b319e3dde02e4cb47827b2
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-21 13:21:09 +0300

    Create 02. Control Structures.py

---
+++ 15. Data Science/02. Control Structures.py
@@ -0,0 +1,43 @@
+# Control Structures: 
+# Sıralı, Seçimli, Tekrarlı olarak üçe ayrılır.
+# Sıralı Kontrol Yapıları
+    # Herhangi bir karşılaştırma işlemi olmaz
+
+# Seçimli Kontrol Yapıları
+    # Dallanma, iki ya da daha fazla alternatif yol arasından seçim yapma durumunda olan problemlerin çözümünde kullanılır.
+    # if, if...else
+
+# Tekrarlı Kontrol Yapıları
+    # Döngü için kullanılan bu yapı, bir kodu arka arkaya bir çok kez çalıştırır.
+    # for loop, while loop
+
+# IF
+# Sonuç açısından iki durum varsa
+point = 80
+if point>85:
+  print("Pass")
+else:
+  print("Fail")     #
+#
+
+# Sonuç açısından ikiden fazla durum varsa
+point = 65
+if point>85:
+  grade = "A"
+elif point>70:
+  grade = "B"
+elif point>60:
+  grade = "C"
+elif point>50:
+  grade = "D"
+else:
+  grade = "F"
+
+print(grade)
+
+
+# Example:
+
+66
+
+
