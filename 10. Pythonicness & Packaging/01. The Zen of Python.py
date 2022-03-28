f76eb06295df923752292bbb0b73804a1c1d06f9
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-03-29 02:42:18 +0300

    Update and rename a.py to 01. The Zen of Python.py

---
+++ 10. Pythonicness & Packaging/01. The Zen of Python.py
@@ -0,0 +1,35 @@
+# SECTION 1: The Zen of Python
+# Writing programs that actually do what they are supposed to do is just one component of being a good Python programmer.
+# It's also important to write clean code that is easily understood, even weeks after you've written it.
+
+# One way of doing this is to follow the Zen of Python, a somewhat tongue-in-cheek set of principles that serves as a guide to programming the Pythoneer way. 
+# Use the following code to access the Zen of Python.
+import this
+
+"""
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+
+https://www.sololearn.com/learning/1073/2482/5184/1 comments
+  
+  
