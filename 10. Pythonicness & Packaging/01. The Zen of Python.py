ddd77fdef2e167b508263f1e62d7c68d2f07569d
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-07 11:10:45 +0300

    Update 01. The Zen of Python.py

--- 10. Pythonicness & Packaging/01. The Zen of Python.py
+++ 10. Pythonicness & Packaging/01. The Zen of Python.py
@@ -30,6 +30,39 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 """
 
-https://www.sololearn.com/learning/1073/2482/5184/1 comments
-  
-  
+# https://github.com/amontalenti/elements-of-python-style#a-little-zen-for-your-code-style
+
+Here are some more... >>> import __hello__ Hello world... >>> >>> import antigravity >>> (opens a comic website) >>> from __future__ import braces File "<stdin>", line 1 SyntaxError: not a chance >>> >>> love=this >>> this is love True >>> love is True False >>> >>> love is False False >>> >>> love is not True or False; love is love True True >>>
+
+"this" is a self-referring key word in many languages- like "self" in Pyhton. And he used it here as self-referring to the zen-essence of the language. oh, this Dutch people, practical, progressive, clear, wise and stingy (read: non-wasteful) : no wonder, such small nation, on such small land has commanded so much of the (past) and current wealth, yet have remained relatively unbombastic.
+
+# SECTION 2
+# Some lines in the Zen of Python may need more explanation.
+# Explicit is better than implicit: It is best to spell out exactly what your code is doing. This is why adding a numeric string to an integer requires explicit conversion, rather than having it happen behind the scenes, as it does in other languages.
+# Flat is better than nested: Heavily nested structures (lists of lists, of lists, and on and on…) should be avoided.
+# Errors should never pass silently: In general, when an error occurs, you should output some sort of error message, rather than ignoring it.
+
+# There are 20 principles in the Zen of Python, but only 19 lines of text.
+# The 20th principle is a matter of opinion, but our interpretation is that the blank line means "Use whitespace".
+# The line "There should be one - and preferably only one - obvious way to do it" references 
+# and contradicts the Perl language philosophy that there should be more than one way to do it.
+
+import this
+help(this)
+
+# https://medium.com/@Pythonidaer/a-brief-analysis-of-the-zen-of-python-2bfd3b76edbf
+
+# Factorial using a 'while loop' 
+num = int(input()) 
+fac = 1 
+while num >=1: 
+  fac *= num 
+  num -= 1 
+  print(fac) 
+#
+# Factorial using 'recursion (in functional programming)'
+# num = int(input()) def factorial(n): if n <= 1: return n else: return n * factorial(n-1) print(factorial(no))
+# Recursion should always be the second thought
+
+
+
