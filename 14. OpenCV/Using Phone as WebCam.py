f5284969475b1061190516a08720447c67d462f0
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-10 03:59:14 +0300

    Create Using Phone as WebCam.py

---
+++ 14. OpenCV/Using Phone as WebCam.py
@@ -0,0 +1,25 @@
+# Import essential libraries
+import requests
+import cv2
+import numpy as np
+import imutils
+
+# Replace the below URL with your own. Make sure to add "/shot.jpg" at last.
+url = "http://192.168.0.103:8080/shot.jpg"
+
+# While loop to continuously fetching data from the URL
+while True:
+  img_resp = requests.get(url)
+  img_arr = np.array(bytarray(img_resp.content), dtype=np.uint8)
+  img = cv2.imdecode(img_arr, -1)
+  img = imutils.resize(img, width=1000, height=1800)
+  cv2.imshow("Android_Cam", img)
+  
+  # Press ESC key to exit
+  if cv2.waitKey(1) == 27:
+    break
+    
+cv2.destroyAllWindows()
+
+
+
