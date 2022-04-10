a530665b977376b6f72f6826607d208108b4e369
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-10 17:50:38 +0300

    Update 06. More on else Statements.py

--- 10. Pythonicness & Packaging/06. More on else Statements.py
+++ 10. Pythonicness & Packaging/06. More on else Statements.py
@@ -19,5 +19,10 @@ else:
 The first for loop executes normally, resulting in the printing of "Unbroken 1".
 The second loop exits due to a break, which is why it's else statement is not executed.
 
+# b equals 1 IF 2+2 is an EXACT MATCH with 5, ELSE b equals 2 2+2 = 4 ≠ 5 So, 2+2 is NOT an EXACT MATCH with 5, so b equals 2
+# if 2 plus 2 equals 5 b is 1 else b is 2
+# b equals to 1, the condition is that if 2+2 ""4"" == 5 else 2 we look it than 4 not equals wich 5 then print (else)
+# b=1 if 2+2==5 else 2 2+2=4 which is not equal to 5 so we are going for the else part which is 2!
 
-https://www.sololearn.com/learning/1073/2500/5195/2  comments
+
+
