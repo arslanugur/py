ad151c2f32b27a1b744686382054b406ea3540c8
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-12 16:15:08 +0300

    Create 09. Major 3rd-Party Libraries.py

---
+++ 10. Pythonicness & Packaging/09. Major 3rd-Party Libraries.py
@@ -0,0 +1,24 @@
+# SECTION 1
+# The Python standard library alone contains extensive functionality.
+# However, some tasks require the use of third-party libraries. Some major third-party libraries:
+# Django: The most frequently used 'web framework' written in Python, Django powers websites that include Instagram and Disqus. 
+# It has many useful features, and whatever features it lacks are covered by extension packages.
+# CherryPy and Flask are also popular web frameworks.
+
+# For scraping data from websites, the library BeautifulSoup is very useful, 
+# and leads to better results than building your own scraper with regular expressions.
+# While Python does offer modules for programmatically accessing websites, such as urllib, they are quite cumbersome to use. 
+# Third-party library requests make it much easier to use HTTP requests.
+
+# A number of third-party modules are available that make it much easier to carry out scientific and mathematical computing with Python.
+# The module matplotlib allows you to create graphs based on data in Python.
+# The module NumPy allows for the use of multidimensional arrays that are much faster than the native Python solution of nested lists. It also contains functions to perform mathematical operations such as matrix transformations on the arrays.
+# The library SciPy contains numerous extensions to the functionality of NumPy.
+
+# Python can also be used for game development.
+# Usually, it is used as a scripting language for games written in other languages, but it can be used to make games by itself.
+# For 3D games, the library Panda3D can be used. For 2D games, you can use pygame.
+
+# http://matthew-brett.github.io/pydagogue/installing_on_debian.html
+
+
