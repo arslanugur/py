59cadae992987b11d7583db42c7a21affcd9daad
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-13 13:01:08 +0300

    Update 10. Packaging.py

--- 10. Pythonicness & Packaging/10. Packaging.py
+++ 10. Pythonicness & Packaging/10. Packaging.py
@@ -3,4 +3,18 @@ This involves use of the modules setuptools and distutils.
 The first step in packaging is to organize existing files correctly. Place all of the files you want to put in a library in the same parent directory. This directory should also contain a file called __init__.py, which can be blank but must be present in the directory.
 This directory goes into another directory containing the readme and license, as well as an important file called setup.py.
 Example directory structure:
-https://www.sololearn.com/learning/1073/2490/5203/1  
+folderA/                  # the Parent folder
+   LICENSE.txt
+   README.txt
+   setup.py
+   folderB/
+      __init__.py
+      codeexample.py
+      codeexample2.py
+# You can place as many script files in the directory as you need.
+
+# this guide is a bit deprecated, because in late Python versions it is not necessary to add __init__.py, 
+# Python 3.3+ has Implicit Namespace Packages which allow it to create a packages without an __init__.py file.
+
+# Which file is placed in a directory to make it a package? ---> __init__.py
+
