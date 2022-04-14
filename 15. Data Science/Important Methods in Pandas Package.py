d40065495bc3ae71312b230d0b7932f2a12b30cd
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-15 02:27:20 +0300

    Create Important Methods in Pandas Package.py

---
+++ 15. Data Science/Important Methods in Pandas Package.py
@@ -0,0 +1,14 @@
+Data Importing          Data Cleaning         Data Statistics
+--------------------------------------------------------------
+pd.read_csv()           df.dropna()           df.head()
+pd.read_table()         df.fillna()           df.tail()
+pd.read_excel()         df.sort_values()      df.info()
+pd.read_sql()           df.groupby()          df.describe()
+pd.read_json()          df.apply()            df.mean()
+pd.read_html()          df.append()           df.median()
+pd.DataFrame()          df.join()             df.std()
+pd.concat()             df.rename()           df.count()
+pd.Series()             df.set_index()        df.max()
+pd.date_range()         df.to_csv()           df.min()
+
+
