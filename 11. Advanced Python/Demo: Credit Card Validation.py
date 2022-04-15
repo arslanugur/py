248b9983ee72908362726100a9e3bd9307aefbe8
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-15 03:30:30 +0300

    Create Demo: Credit Card Validation.py

---
+++ 11. Advanced Python/Demo: Credit Card Validation.py
@@ -0,0 +1,20 @@
+def validate(card_number):
+  def digits_of(n):
+    return [int(d) for d in str(n)]
+  digits = digits_of(card_number)
+  odd_digits = digits[-1::-2]
+  even_digits = digits[-2::-2]
+  checksum = 0
+  checksum += sum(odd_digits)
+  for d in even_digits:
+    checksum += sum(digits_of(d*2))
+  return checksum % 10
+    
+if validate(input("Enter Number \n"))==0:
+  print("valid")
+else:
+  print("Invalid")
+#
+
+
+
