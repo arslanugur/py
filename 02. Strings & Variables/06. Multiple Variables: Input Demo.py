894abf9b47fc4456a2fc69b2ecab6eeee67c9d48
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-15 23:31:30 +0300

    Create 06. Multiple Variables: Input Demo.py

---
+++ 02. Strings & Variables/06. Multiple Variables: Input Demo.py
@@ -0,0 +1,22 @@
+# Multiple Variables
+
+# You can use multiple variables to take multiple inputs for your program.
+# For example, the following code takes two inputs and stores them in the variables x and y:
+x = input()
+y = input()
+
+# Task:
+# Given the code above, output the input x repeated y times.
+
+# Sample Input:
+# awesome
+# 3
+
+# Sample Output:
+# awesomeawesomeawesome
+
+# Code:
+x = input()
+y = int(input())
+print(x * y)
+
