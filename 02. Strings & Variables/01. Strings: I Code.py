b27a862e594fe505e535ab5e04f9ba8e414c918b
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-15 20:46:51 +0300

    Create 01. Strings: I Code.py

---
+++ 02. Strings & Variables/01. Strings: I Code.py
@@ -0,0 +1,12 @@
+# Example: Strings
+
+# Fix the given code to output I'm a programmer.
+
+# Hint
+# Escape the single quote inside the string.
+# Use backslash \ for escaping.
+
+# Example:
+print('I\'m a programmer')
+
+
