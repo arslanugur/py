0cb099dd27c0a989898e0a7711b9ec8625016d8c
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-15 23:26:37 +0300

    Create 04. Variables: Demo.py

---
+++ 02. Strings & Variables/04. Variables: Demo.py
@@ -0,0 +1,12 @@
+# Variables
+
+# The provided code stores the value 7 in a variable, and outputs it.
+
+# Task
+# Change the code to output the value of the variable raised to the power of 3.
+# Exponentiation can be used to raise a number to the power of another.
+
+# Code:
+x = 7
+print(x**3)
+
