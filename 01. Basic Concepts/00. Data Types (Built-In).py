d258ad87d8f02f7d9cfa8a8b063e05cecdf58e4d
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-17 01:06:13 +0300

    Create 00. Data Types (Built-In).py

---
+++ 01. Basic Concepts/00. Data Types (Built-In).py
@@ -0,0 +1,15 @@
+str              x = "python"
+int              x = 17
+float            x = 17.5
+complex          x = 7j
+list             x = ["green", "blue"]
+tuple            x = ("green", "blue")
+range            range(6)
+dict             x = {"red":"1"}
+set              x = {"green","blue"}
+frozenset        x = frozenset({"a","b"})
+bool             x = True
+bytes            x = z"python"
+bytearray        x = bytearray(5)
+memoryview       x = memoryview(bytes(5))
+
