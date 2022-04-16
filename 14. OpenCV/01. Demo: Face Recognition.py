bcc041a01864fbec195258e56728075cffe97122
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-16 10:30:14 +0300

    Update 01. Demo: Face Recognition.py

--- 14. OpenCV/01. Demo: Face Recognition.py
+++ 14. OpenCV/01. Demo: Face Recognition.py
@@ -10,12 +10,39 @@ while True:
   
   for x, y, w, h in faces_:
     cv2.rectangle(square_, (x,y), (x + w, y + h), (255, 0, 0), 2)
-  cv2.imshow('square', square_)
+  cv2.imshow('square_', square_)
   k = cv2.waitKey(1) & 0xff
   if k == 27 or k == ord('q'):
     break
     
 camera_.release()
 cv2.destroyAllWindows()
+
+
+
+# Second Way
+# pip install opencv-python       # Kütüphane kurulumu için
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')     # Bu link indirilmesi gerek
+
+# Read the input image
+img = cv2.imread('tests.jpg')
+
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect faces
+faces = face_cascade.detectMultiScale(gray,  1.1, 4)
+
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+  cv2.rectangle(img, (x,y), (x + w, y + h), (255, 0, 0), 2)
+  
+# Display the output
+cv2.imshow('img', img)
+cv2.waitKey()
+
 
 
