757d4c6e2ad1f7d794fadb86d3f70b0bbac83086
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-27 02:04:30 +0300

    Update Classification Model - K-Nearest Neighbor.py

--- Machine Learning/Classification Model - K-Nearest Neighbor.py
+++ Machine Learning/Classification Model - K-Nearest Neighbor.py
@@ -1,1 +1,49 @@
+# import required library
+import numpy as np
+import maplotlib.pyplot as plt
+import pandas as pd
 
+# load the datasets and split into terget variableand attributes
+dataset = pd.read_csv('Social_Network_Ads.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# splitting the dataset into the training set and test set
+from sklearn.model_selection import train_set_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 0)
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+# train the KNN model on the training dataset
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p=2)
+classifier.fit(x_train, y_train)
+
+# predicting the test set results
+y_pred = classifier.predict(x_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+# making the confusion matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
+
+# visualising the training set results
+from matplotlib.colors import ListedColormap
+x_set, y_set = sc.inverse_transform(x_train), y_train
+x1, x2 = np.meshgrid(np.arange(start = x_set[:,0].min() - 10, stop = x_set[:, 0].max() +10, step=1),
+                     np.arange(start = x_set[:,1].min() - 1000, stop = x_set[:, 1].max() +1000, step=1)
+plt.contourf(x1, x2, classifier.predict(sc.transform(np.array([x1.ravel(), x2.ravel()]).T)).reshape(x1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(x1.min(), x1.max())
+plt.xlim(x2.min(), x2.max())
+
+# visualising the test set results
+
+
+# Dataset source: https://www.kaggle.com/rakeshrau/social-network-ads
