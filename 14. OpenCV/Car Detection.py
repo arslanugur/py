19606ebd8cae36619cd66be8d0717bd8f24f1ba1
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-30 22:45:08 +0300

    Update Car Detection.py

--- 14. OpenCV/Car Detection.py
+++ 14. OpenCV/Car Detection.py
@@ -45,27 +45,39 @@ detectCars(video)
 
 
 # Second Code:
+# OpenCV Python program to detect cars in video frame
+# import libraries of python OpenCV
 import cv2
 
-cap  = cv2.VideoCapture('video.mp4')
+#capture frames from a video
+cap  = cv2.VideoCapture('samplecarvideo.mp4')
 
+# Trained XML classifiers describes some features of some object we want to detect
 car_cascade = cv2.CascadeClassifier('cars.xml')
 
+# loop runs if capturing has been initialized.
 while True:
+    # reads frames from a video
     ret, frames = cap.read()
     
+    # convert to gray scale of each frames    
     gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
     
+    # Detects cars of different sizes in the input image
     cars = cars_cascade = car_cascade.detectMultiScale(gray, 1.1, 1)
     
+    # To draw a rectangle in each cars
     for (x,y,w,h) in cars:
         cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
         
+    # Display frames in a window
     cv2.imshow('video2', frames)
     
+    # Wait for Esc key to stop    
     if cv2.waitKey(33) == 27:
         break
 
-cv2.destroyAllWindows()
+# De-allocate any associated memory usage
+cv2.destroyAllwindows()
 
 
