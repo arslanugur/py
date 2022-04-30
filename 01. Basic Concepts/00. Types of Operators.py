aae5c176226d07fe6a61203068cb8c69ed5015c2
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-30 18:25:22 +0300

    Update 00. Types of Operators.py

--- 01. Basic Concepts/00. Types of Operators.py
+++ 01. Basic Concepts/00. Types of Operators.py
@@ -7,6 +7,21 @@
   **      exponentiation
   //      floor division
   
+x, y = 3,2        # Output
+print(x + y)      # 5
+print(x - y)      # 1
+print(x * y)      # 6
+print(x, y)       # 1.5
+print(x // y)     # 1
+print(x % y)      # 1
+print(-x)         # -3
+print(abs(-x))    # 3
+print(int(3.9))   # 3
+print(float(x))   # 3.0   
+print(x ** y)     # 9
+  
+  
+  
 - Assignment Operators
   =
   +=
