e8be0fb44082ca16035a748160702189e1e2161c
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-04-30 22:30:23 +0300

    Update 00. Function vs Method.py

--- 04. Functions & Modules/00. Function vs Method.py
+++ 04. Functions & Modules/00. Function vs Method.py
@@ -5,6 +5,12 @@
 # 3. Functions are not associated with any class or object of a class
 # 4. Py has many inbuilt functions such as sorted(), int(), float(), max(), min() et cetera
 
+# Function is block of code that is also called by its name. (independent)
+# The function can have different parameters or may not have any at all.
+# If any data (parameters) are passed, they are passed explicitly.
+# It may or may not return any data.
+# Function does not deal with Class and its instance concept.
+
 # Syntax:
 def function(par1, par2, par n):
   # statement
@@ -26,12 +32,23 @@ print(even(5))
 # 2 is an even number
 # 5 is not an even number
 
+# Code:
+def add(a, b):
+  return (a+b)
+print (add (10, 12))  # 22
+
+
 
 # METHODS
 # 1. Method is also similar to function which is called dependently by its name associating to an object
 # 2. Method is implicitly passed the object on which it is invoked
 # 3. Methods are associated with an object of a class
 # 4. Py has many inbuilt methods such as randint(), sqrt(), randrange() et cetera
+
+# Method is called by its name, but it is associated to an object (dependent).
+# A method is implicitly passed the object on which it is invoked.
+# It may or may not return any data.
+# A method can operate on the data (instance variables) that is contained by the corresponding class
 
 # Syntax:
 class class_name
@@ -58,5 +75,18 @@ print(obj.even_numbers(5))
 # output:
 # 2 is an even number
 # 5 is not an even number
+
+
+# Code:
+class Add:
+  def add_method (self, a,b):
+    self.a = a
+    self.b = b
+    print (a+b)
+
+opr = Add()
+opr.add_method (10, 12)  # 22
+
+
 
 
