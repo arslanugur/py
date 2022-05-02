e41ee433d660a240e844263dc6e7faa509e881f7
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-05-02 04:38:47 +0300

    Update Classification Model - K-Nearest Neighbor.py

--- Machine Learning/Classification Model - K-Nearest Neighbor.py
+++ Machine Learning/Classification Model - K-Nearest Neighbor.py
@@ -44,6 +44,21 @@ plt.xlim(x1.min(), x1.max())
 plt.xlim(x2.min(), x2.max())
 
 # visualising the test set results
-
+from matplotlib.colors import ListedColormap
+x_set, y_set = sc.inverse_transform(x_test), y_test
+x1, x2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 10, stop = x_set[:, 0].max() + 10, step = 1),
+                     np.arange(start = x_set[:, 1].min() - 1000, stop = x_set[:, 1].max() + 1000, step = 1))
+plt.contourf(x1, x2, classifier.predict(sc.transform(np.array([x1.ravel()]).T)).reshape(x1.shape), alpha = 0.75, cmap = ListedColormap(('red','green')))
+plt.xlim(x1.min(), x1.max())
+plt.ylim(x2.min(), x2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j,1], c = ListedColormap(('red','green'))(i), label = j)
+plt.title('K-NN(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
 
 # Dataset source: https://www.kaggle.com/rakeshrau/social-network-ads
+
+
