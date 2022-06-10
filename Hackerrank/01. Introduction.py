45ed69f33ae31518309977ffa8a839918423fdc6
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-10 18:32:35 +0300

    Create 01. Introduction.py

---
+++ Hackerrank/01. Introduction.py
@@ -0,0 +1,26 @@
+# 01. Say "Hello, World!" With Python
+if __name__ == '__main__':
+    print("Hello, World!")
+
+
+
+# 02. Python If-Else
+if __name__ == '__main__':
+    n = int(input())
+    if n % 2 == 1 or 6 <= n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+
+
+
+# 03. Arithmetic Operators
+
+# 04. Python: Division
+
+# 05. Loops
+
+# 06. Print Function
+
+# 07. Write a Function
+
