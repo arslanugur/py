09bc0a45e06ee428b10225cb99c9199511200f34
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-11 23:10:16 +0300

    Update 01. Introduction.py

--- Hackerrank/01. Introduction.py
+++ Hackerrank/01. Introduction.py
@@ -15,12 +15,50 @@ if __name__ == '__main__':
 
 
 # 03. Arithmetic Operators
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+
 
 # 04. Python: Division
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a//b)
+    print(a/b)
+
+
 
 # 05. Loops
+if __name__ == '__main__':
+    n = int(input())
+
+    num = 0
+    while num < n:
+        print(pow(num, 2))
+        num += 1
+
+
 
 # 06. Print Function
+if __name__ == '__main__':
+    n = int(input())
+    
+    num = 1
+    while num <= n:
+        print(num, end='')
+        num += 1
+
+
 
 # 07. Write a Function
+def is_leap(year):
+    return (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0))
+
 
