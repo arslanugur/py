2fb89e9ef47b66dcb796e01aef7e1a06102c9916
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-14 18:04:07 +0300

    Update 03. Strings.py

--- Hackerrank/03. Strings.py
+++ Hackerrank/03. Strings.py
@@ -26,12 +26,66 @@ def print_full_name(a, b):
 
 
 # 04. Mutations
+def mutate_string(string, position, character):
+    out = list(string)
+    out[position] = character
+    return "".join(out)
+
+
 
 # 05. Find a string
+def count_substring(string, sub_string):
+    res = 0
+    len_sub = len(sub_string)
+    
+    for i in range(len(string) - len_sub + 1):
+        if string[i:i + len_sub] == sub_string:
+            res += 1
+        i += 1
+        
+    return res
+
+
 
 # 06. String Validators
+if __name__ == '__main__':
+    s = input()
+    
+    print(any(char.isalnum() for char in s))
+    print(any(char.isalpha() for char in s))
+    print(any(char.isdigit() for char in s))
+    print(any(char.islower() for char in s))
+    print(any(char.isupper() for char in s))
+
+
 
 # 07. Text Alignment
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+
+
 
 # 08. Text Wrap
 
