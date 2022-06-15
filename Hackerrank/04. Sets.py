27886e74cbac17c468e3662de73238a3f8a06e4e
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-15 18:06:53 +0300

    Create 04. Sets.py

---
+++ Hackerrank/04. Sets.py
@@ -0,0 +1,25 @@
+# 01. Introduction to Sets
+
+# 02. Symmetric Difference
+
+# 03. Set .add()
+
+# 04. Set .discard(), .remove() & .pop()
+
+# 05. Set .union() Operation
+
+# 06. Set .intersection() Operation
+
+# 07. Set .difference() Operation
+
+# 08. Set .symmetric_difference() Operation
+
+# 09. Set Mutations
+
+# 10. The Captain's Room
+
+# 11. Check Subset
+
+# 12. Check Strict Superset
+
+# 13. No Idea!
