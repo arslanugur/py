dfd16559e01535bf0e70cff9888a82e604df4820
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-16 18:09:22 +0300

    Update 04. Sets.py

--- Hackerrank/04. Sets.py
+++ Hackerrank/04. Sets.py
@@ -1,10 +1,61 @@
 # 01. Introduction to Sets
+def average(array):
+    uniq = set(array)
+    
+    return sum(uniq)/len(uniq)
+
+
 
 # 02. Symmetric Difference
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    M = int(input().strip())
+    set_m = set(map(int, input().strip().split(' ')))
+    
+    N = int(input().strip())
+    set_n = set(map(int, input().strip().split(' ')))
+    
+    for el in sorted(set_m ^ set_n):
+        print(el)
+
+
 
 # 03. Set .add()
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    N = int(input().strip())
+    stamps = set()
+    
+    for _ in range(N):
+        stamp = input().strip()
+        stamps.add(stamp)
+        
+    print(len(stamps))
+
+
 
 # 04. Set .discard(), .remove() & .pop()
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    n = int(input())
+    s = set(map(int, input().split())) 
+    num_cmds = int(input())
+    
+    for _ in range(num_cmds):
+        cmd = list(input().strip().split(' '))
+        if cmd[0] == 'pop':
+            s.pop()
+        elif cmd[0] == 'remove':
+            s.remove(int(cmd[1]))
+        elif cmd[0] == 'discard':
+            s.discard(int(cmd[1]))
+    
+    print(sum(s))
+
+
 
 # 05. Set .union() Operation
 
