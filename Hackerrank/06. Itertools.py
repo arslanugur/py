3ecc4ece43635a281c3204b6c770893e2ab84329
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-06-18 21:39:48 +0300

    Update 06. Itertools.py

--- Hackerrank/06. Itertools.py
+++ Hackerrank/06. Itertools.py
@@ -1,1 +1,25 @@
+# 01. itertools.product()
+#!/usr/bin/env python3
 
+from itertools import product
+
+if __name__ == "__main__":
+    arr1 = list(map(int, input().strip().split(' ')))
+    arr2 = list(map(int, input().strip().split(' ')))
+    
+    for el in product(arr1, arr2):
+        print("{} ".format(el), end='')
+
+
+
+# 02. itertools.permutations()
+
+# 03. itertools.combinations()
+
+# 04. itertools.combinations_with_replacement()
+
+# 05. Compress the String!
+
+# 06. Iterables and Iterators
+
+# 07. Maximize It!
