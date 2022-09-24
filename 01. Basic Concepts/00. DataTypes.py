bfed28e296760154356dd087b0665bb6c6befbfd
Arslan <45125597+arslanugur@users.noreply.github.com>
2022-09-25 02:51:00 +0300

    Create 00. DataTypes.py

---
+++ 01. Basic Concepts/00. DataTypes.py
@@ -0,0 +1,10 @@
+Datatypes               Mutable   Indexed   Ordered   Duplicate
+---------------------------------------------------------------
+List []                 V         V         V         V
+Tuple ()                X         V         V         V
+Set {}                  V         X         X         X
+Frozenset {}            X         X         X         X
+Dictionary {K:V}        V         V         V         X
+Bytearray b'\x011\x03'  V         V         V         V
+
+
